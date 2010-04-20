59df586d59236a6e2356bdfe6db29c5987447902
Marc Fournier <marc.fournier@camptocamp.org>
2010-04-20 10:23:23 +0000

    imported a bunch of useful puppet modules
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2489 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/modules/sync-modules.sh
+++ puppetmaster/modules/sync-modules.sh
@@ -7,7 +7,29 @@ echo -e "
 
 git://github.com/camptocamp/puppet-common.git common origin/master
 git://github.com/camptocamp/puppet-apt.git apt origin/master
+git://github.com/camptocamp/puppet-openssl.git openssl origin/master
+git://github.com/camptocamp/puppet-rsyncd.git rsyncd origin/master
 git://github.com/camptocamp/puppet-postfix.git postfix origin/master
+git://github.com/bodepd/puppet-sudo.git sudo origin/master
+git://puppet-modules.git.puzzle.ch/module-sysctl.git sysctl origin/master
+
+git://github.com/camptocamp/puppet-pacemaker.git pacemaker origin/master
+git://github.com/camptocamp/puppet-varnish.git varnish origin/master
+git://github.com/camptocamp/puppet-haproxy.git haproxy origin/master
+
+git://github.com/camptocamp/puppet-apache.git apache origin/master
+git://github.com/camptocamp/puppet-postgresql.git postgresql origin/master
+git://github.com/camptocamp/puppet-mysql.git mysql origin/master
+
+git://github.com/camptocamp/puppet-collectd.git collectd origin/master
+git://github.com/camptocamp/puppet-nagios.git nagios origin/refactorisation
+
+git://github.com/camptocamp/puppet-python.git python origin/master
+git://github.com/camptocamp/puppet-ruby.git ruby origin/master
+git://github.com/camptocamp/puppet-perl.git perl origin/master
+git://github.com/camptocamp/puppet-php.git php origin/master
+git://github.com/camptocamp/puppet-java.git java origin/master
+git://github.com/camptocamp/puppet-buildenv.git buildenv origin/master
 
 " | egrep -v '^$|^#' | while read item; do
 
