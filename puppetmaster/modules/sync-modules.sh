0cd4eb072179876b9fe26e90609fe6029e7da1fc
Marc Fournier <marc.fournier@camptocamp.org>
2010-04-30 20:08:26 +0000

    régression dans le module apt. standby en attendant un correctif.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2528 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/modules/sync-modules.sh
+++ puppetmaster/modules/sync-modules.sh
@@ -6,7 +6,7 @@ echo -e "
 # URL<one space>modulename<one space>commit<newline>
 
 git://github.com/camptocamp/puppet-common.git common origin/master
-git://github.com/camptocamp/puppet-apt.git apt origin/master
+git://github.com/camptocamp/puppet-apt.git apt ed1e916173e
 git://github.com/camptocamp/puppet-openssl.git openssl origin/master
 git://github.com/camptocamp/puppet-rsyncd.git rsyncd origin/master
 git://github.com/camptocamp/puppet-postfix.git postfix origin/master
