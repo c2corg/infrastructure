aa960501c43bc28e4182e313fd4a4ac6e289180e
Marc Fournier <marc.fournier@camptocamp.org>
2010-08-06 16:49:10 +0000

    use more up to date iptables module.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2863 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/modules/sync-modules.sh
+++ puppetmaster/modules/sync-modules.sh
@@ -11,7 +11,7 @@ git://github.com/camptocamp/puppet-openssl.git openssl origin/master
 git://github.com/camptocamp/puppet-rsyncd.git rsyncd origin/master
 git://github.com/camptocamp/puppet-postfix.git postfix origin/master
 git://github.com/camptocamp/puppet-sysctl.git sysctl origin/master
-git://github.com/camptocamp/puppet-iptables.git iptables origin/master
+git://github.com/kbarber/puppet-iptables.git iptables origin/master
 git://github.com/bodepd/puppet-sudo.git sudo origin/master
 git://github.com/bruce/puppet-vcsrepo.git vcsrepo origin/master
 git://github.com/reductivelabs/puppet-lvm.git lvm origin/master
