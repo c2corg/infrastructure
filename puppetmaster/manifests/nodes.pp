28dd20de517a021774a9fcf062c905cad0ae8566
Marc Fournier <marc.fournier@camptocamp.org>
2010-09-29 23:48:41 +0000

    haproxy basic config.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2987 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/manifests/nodes.pp
+++ puppetmaster/manifests/nodes.pp
@@ -118,7 +118,31 @@ node 'hn2' inherits 'base-node' {
 
 node 'test-marc' inherits 'base-node' {
 
-  include haproxy
+  package { "haproxy":
+    ensure => present
+  }
+
+  service { "haproxy":
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+    restart   => "/etc/init.d/haproxy reload",
+    require   => Package["haproxy"],
+  }
+
+  augeas { "enable haproxy" :
+    context => "/files/etc/default/haproxy",
+    changes => "set ENABLED 1",
+    notify  => Service["haproxy"],
+  }
+
+  file { "/etc/haproxy/haproxy.cfg":
+    ensure => present,
+    owner  => "root",
+    mode   => 0644,
+    source => "puppet:///c2corg/ha/haproxy.cfg",
+    notify => Service["haproxy"],
+  }
 
 }
 
