d4fe60a85f9fb6f5ff5980085a791391682b93af
Marc Fournier <marc.fournier@camptocamp.org>
2010-11-16 20:23:27 +0000

    c2corg::database - setup pg_hba.conf using postgresql::hba.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3232 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/local/c2corg/manifests/database.pp
+++ puppetmaster/local/c2corg/manifests/database.pp
@@ -25,19 +25,27 @@ class c2corg::database::base {
 }
 
 class c2corg::database::prod inherits c2corg::database::base {
+
+  augeas { "passwd auth for local postgres connections":
+    context   => "/files/etc/postgresql/8.4/main/",
+    changes   => "set pg_hba.conf/*[type='local'][user='all'][database='all']/method md5",
+    load_path => "/usr/share/augeas/lenses/contrib/",
+    notify    => Service["postgresql"],
+  }
+
+  postgresql::hba { "access to sympa user":
+    ensure   => present,
+    type     => 'host',
+    database => 'c2corg',
+    user     => 'sympa',
+    address  => '192.168.0.0/16',
+    method   => 'md5',
+    pgver    => '8.4',
+  }
+
 }
 
 class c2corg::database::dev inherits c2corg::database::base {
-
-  file { "/etc/postgresql/8.3/main/pg_hba.conf":
-    ensure => "present",
-    owner => "postgres",
-    group => "postgres",
-    mode => "0640",
-    content => "local all all trust\nhost all all 127.0.0.1/32 trust\n",
-    notify => Service["postgresql-8.3"],
-    require => Package["postgresql-8.3"],
-  }
 
 }
 
