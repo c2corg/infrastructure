d4f90bbf7d23eac90f8b1ec2e5fcd36e417f3ca4
Marc Fournier <marc.fournier@camptocamp.org>
2010-12-16 21:21:14 +0000

    gnupg::privkey generate private keys.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3373 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/local/gnupg/manifests/init.pp
+++ puppetmaster/local/gnupg/manifests/init.pp
@@ -25,3 +25,25 @@ define gnupg::pubkey ($ensure="present", $user) {
 
   }
 }
+
+define gnupg::privkey (
+  $user,
+  $keytype='1',
+  $keylength='1024',
+  $realname='',
+  $passphrase='',
+  $expiry='0') {
+
+  $template = template("gnupg/unattended-key-gen.erb")
+
+  exec { "generate private key $name for $user":
+    command   => "cat << EOF | su -c 'gpg --gen-key --batch' $user
+$template
+EOF",
+    unless    => "su -c 'gpg --list-secret-keys $name' $user",
+    logoutput => true,
+    timeout   => 0,
+  }
+
+}
+
