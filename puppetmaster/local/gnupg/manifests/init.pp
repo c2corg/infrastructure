4153a3f064c412c733f2e936b3759c94f607e95b
Marc Fournier <marc.fournier@camptocamp.org>
2010-12-16 21:21:14 +0000

    module pour gnupg.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3372 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ puppetmaster/local/gnupg/manifests/init.pp
@@ -0,0 +1,27 @@
+class gnupg {
+
+  package { "gnupg": ensure => present }
+
+}
+
+define gnupg::pubkey ($ensure="present", $user) {
+
+  case $ensure {
+
+    present: {
+      exec { "import gpg key $name for $user":
+        command => "su -c 'gpg --batch --keyserver pgp.mit.edu --recv-keys $name' $user",
+        unless  => "su -c 'gpg --list-keys $name' $user",
+        timeout => 0,
+      }
+    }
+
+    absent: {
+      exec { "delete gpg key $name for $user":
+        command => "su -c 'gpg --batch --yes --delete-keys $name' $user",
+        onlyif  => "su -c 'gpg --list-keys $name' $user",
+      }
+    }
+
+  }
+}
