a7da2d0695c9a8ed398424842526a33c4903bef6
Marc Fournier <marc.fournier@camptocamp.org>
2010-12-16 21:21:16 +0000

    fixed various dependencies and other gotchas.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3378 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/local/c2corg/manifests/reprepro.pp
+++ puppetmaster/local/c2corg/manifests/reprepro.pp
@@ -1,35 +1,45 @@
 class c2corg::reprepro {
 
   $reprepro_basedir = "/srv/deb-repo"
-  include reprepro
+  include reprepro::debian
+
+  file { "${reprepro_basedir}/.ssh/":
+    ensure => directory,
+    owner  => "reprepro",
+    group  => "reprepro",
+  }
 
   # allowed uploader(s)
   account::user { "marc@reprepro":
     user    => "marc",
     account => "reprepro",
+    require => File[$reprepro_basedir],
   }
 
   # reprepro only validates packages signed by known keys.
   gnupg::pubkey { "4DA7C546":
-    user => "reprepro",
+    user    => "reprepro",
+    require => [User["reprepro"], File[$reprepro_basedir]],
   }
 
   # generate a passwordless GPG key...
   gnupg::privkey { "pkgs@c2corg":
-    user => "reprepro",
+    user     => "reprepro",
     realname => "c2corg Package Archive Key",
+    require  => [User["reprepro"], File[$reprepro_basedir]],
   }
 
   # ...and publish the public part.
   gnupg::pubkey::file { "pkgs@c2corg":
-    user => "reprepro",
-    file => "${reprepro_basedir}/pubkey.txt",
+    user     => "reprepro",
+    file     => "${reprepro_basedir}/pubkey.txt",
+    require  => [User["reprepro"], File[$reprepro_basedir]],
   }
 
   /* configure the repository managment tool */
 
   reprepro::repository { "test": incoming_allow => "lenny squeeze" }
-  reprepro::repository { "prod": # no incoming, just feed on test repo }
+  reprepro::repository { "prod": } # no incoming, just feed on test repo
 
   Reprepro::Distribution {
     origin        => "C2corg",
@@ -87,7 +97,7 @@ chroot
 dir=$reprepro_basedir
 port=80
 ",
-    require => Package["thttpd"],
+    require => [Package["thttpd"], File[$reprepro_basedir]],
     notify  => Service["thttpd"],
   }
 
