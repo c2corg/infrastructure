1e8f325bb23e63aeffb55d6cdb2b18068d6c8c5b
Marc Fournier <marc.fournier@camptocamp.org>
2010-12-16 21:21:15 +0000

    gnupg::pubkey::file - export gpg public keys.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3374 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/local/gnupg/manifests/init.pp
+++ puppetmaster/local/gnupg/manifests/init.pp
@@ -47,3 +47,17 @@ EOF",
 
 }
 
+define gnupg::pubkey::file ($user, $ascii=true, $file) {
+
+  if ($ascii == true) {
+    $opt = "--armor"
+  } else {
+    $opt = ""
+  }
+
+  exec { "export key $name":
+    command => "su -c 'gpg $opt --export $name > $file' $user",
+    unless  => "test -e $file",
+  }
+
+}
