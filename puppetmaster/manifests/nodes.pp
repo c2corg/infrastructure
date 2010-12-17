8a93efa35b990e9f9ecb4054d87de1ecb432314c
Marc Fournier <marc.fournier@camptocamp.org>
2010-12-17 09:13:01 +0000

    cleanup: moved node resources to c2corg::hn classes.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3387 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/manifests/nodes.pp
+++ puppetmaster/manifests/nodes.pp
@@ -31,72 +31,20 @@ node 'pkg' inherits 'base-node' {
 # ProLiant DL360 G4p - debian/squeeze
 node 'hn0' inherits 'base-node' {
 
+  include c2corg::hn::hn0
+
   include vz
   include c2corg::vz
-
-  iptables { "setup nat for private LAN":
-    table    => "nat",
-    proto    => "all",
-    chain    => "POSTROUTING",
-    outiface => "eth2",
-    source   => "192.168.192.0/24",
-    jump     => "MASQUERADE",
-    require  => Sysctl::Set_value["net.ipv4.ip_forward"],
-  }
-
-  iptables { "setup nat for VZ nodes":
-    table    => "nat",
-    proto    => "all",
-    chain    => "POSTROUTING",
-    outiface => "eth2",
-    source   => "192.168.191.0/24",
-    jump     => "MASQUERADE",
-    require  => Sysctl::Set_value["net.ipv4.ip_forward"],
-  }
-
-  vz::fwd { "forward hn1 ssh port":
-    net  => "192.168.192",
-    ve   => "2",
-    from => "20022",
-    to   => "22",
-  }
-
-  vz::fwd { "forward hn2 ssh port":
-    net  => "192.168.192",
-    ve   => "3",
-    from => "20023",
-    to   => "22",
-  }
-
-  file { "/etc/network/if-pre-up.d/iptables":
-    ensure  => present,
-    mode    => 0755,
-    content => "#!/bin/sh
-# file managed by puppet
-
-if [ -e /etc/iptables.rules ]; then
-  /sbin/iptables-restore /etc/iptables.rules
-fi
-",
-  }
-
-  file { "/etc/network/interfaces":
-    ensure => present,
-    source => "puppet:///c2corg/network/hn0",
-  }
 
 }
 
 # ProLiant DL360 G4 - debian/kFreeBSD
 node 'hn1' inherits 'base-node' {
 
+  include c2corg::hn::hn1
+
   include puppet::devel
   include varnish
-
-  file { "/etc/network/interfaces":
-    ensure => present,
-    source => "puppet:///c2corg/network/hn1",
-  }
 
   varnish::instance { $hostname:
     vcl_file   => "puppet:///c2corg/ha/c2corg.vcl",
@@ -108,22 +56,9 @@ node 'hn1' inherits 'base-node' {
 # PowerEdge 2950 - debian/lenny
 node 'hn2' inherits 'base-node' {
 
+  include c2corg::hn::hn2
+
   include haproxy
-
-  file { "/etc/network/interfaces":
-    ensure => present,
-    source => "puppet:///c2corg/network/hn2",
-  }
-
-  augeas { "enable console on serial port":
-    context => "/files/etc/inittab/T0/",
-    changes => [
-      "set runlevels 12345",
-      "set action respawn",
-      "set process '/sbin/getty -L ttyS0 115200 vt100'"
-    ],
-    notify  => Exec["refresh init"],
-  }
 
 }
 
---
+++ puppetmaster/site-modules/c2corg/manifests/hn.pp
@@ -0,0 +1,78 @@
+class c2corg::hn {
+
+  file { "/etc/network/interfaces":
+    ensure => present,
+    source => "puppet:///c2corg/network/${hostname}",
+  }
+
+}
+
+/* openvz host */
+class c2corg::hn::hn0 inherits c2corg::hn {
+
+  iptables { "setup nat for private LAN":
+    table    => "nat",
+    proto    => "all",
+    chain    => "POSTROUTING",
+    outiface => "eth2",
+    source   => "192.168.192.0/24",
+    jump     => "MASQUERADE",
+    require  => Sysctl::Set_value["net.ipv4.ip_forward"],
+  }
+
+  iptables { "setup nat for VZ nodes":
+    table    => "nat",
+    proto    => "all",
+    chain    => "POSTROUTING",
+    outiface => "eth2",
+    source   => "192.168.191.0/24",
+    jump     => "MASQUERADE",
+    require  => Sysctl::Set_value["net.ipv4.ip_forward"],
+  }
+
+  vz::fwd { "forward hn1 ssh port":
+    net  => "192.168.192",
+    ve   => "2",
+    from => "20022",
+    to   => "22",
+  }
+
+  vz::fwd { "forward hn2 ssh port":
+    net  => "192.168.192",
+    ve   => "3",
+    from => "20023",
+    to   => "22",
+  }
+
+  file { "/etc/network/if-pre-up.d/iptables":
+    ensure  => present,
+    mode    => 0755,
+    content => "#!/bin/sh
+# file managed by puppet
+
+if [ -e /etc/iptables.rules ]; then
+  /sbin/iptables-restore /etc/iptables.rules
+fi
+",
+  }
+
+}
+
+/* varnish server */
+class c2corg::hn::hn1 inherits c2corg::hn {
+}
+
+/* webserver & database */
+class c2corg::hn::hn2 inherits c2corg::hn {
+
+  augeas { "enable console on serial port":
+    context => "/files/etc/inittab/T0/",
+    changes => [
+      "set runlevels 12345",
+      "set action respawn",
+      "set process '/sbin/getty -L ttyS0 115200 vt100'"
+    ],
+    notify  => Exec["refresh init"],
+  }
+
+}
