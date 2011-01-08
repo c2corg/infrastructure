6cdadf49ba81049d1fc09e83ce499b6e745cd7bc
Marc Fournier <marc.fournier@camptocamp.org>
2011-01-08 21:22:44 +0000

    configured drraw.cgi
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3469 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/collectd.pp
+++ puppetmaster/site-modules/c2corg/manifests/collectd.pp
@@ -35,19 +35,21 @@ class c2corg::collectd::client {
 
 class c2corg::collectd::server inherits c2corg::collectd::client {
 
+  $datadir = '/srv/collectd'
+
   resources { collectd_conf: purge => true; }
 
-  file { '/srv/collectd':
+  file { $datadir:
     ensure => directory,
   }
 
   # TODO: tidy stall files in /srv/collectd
 
   Collectd::Rrdtool['rrd'] {
-    datadir           => '"/srv/collectd"',
+    datadir           => "\"${datadir}\"",
     cache_flush       => 900,
     cache_timeout     => 10,
-    require           => File['/srv/collectd'],
+    require           => File[$datadir],
   }
 
   Collectd::Network["network"] {
@@ -58,6 +60,25 @@ class c2corg::collectd::server inherits c2corg::collectd::client {
   # visualisation stuff
 
   include thttpd
+
+  file { "/var/www/cgi-bin/":
+    ensure  => directory,
+    require => Package["thttpd"],
+  }
+
   package { 'drraw': require => Package['thttpd'] }
+
+  line { "configure drraw rrd path":
+    line    => "%datadirs = ( \"${datadir}\"  => \"[c2corg]\" );",
+    file    => "/etc/drraw/drraw.conf",
+    require => Package["drraw"],
+  }
+
+  file { "/var/www/cgi-bin/drraw.cgi":
+    mode    => 0755,
+    ensure  => directory,
+    source  => "file:///usr/lib/cgi-bin/drraw/drraw.cgi",
+    require => Package["drraw"],
+  }
 
 }
