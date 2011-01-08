a4eadf37ce80d36116067138695d15e246f7d94e
Marc Fournier <marc.fournier@camptocamp.org>
2011-01-08 21:22:44 +0000

    proxypasses to misc admin stuff.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3470 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/manifests/nodes.pp
+++ puppetmaster/manifests/nodes.pp
@@ -140,6 +140,49 @@ node 'pre-prod' inherits 'base-node' {
   c2corg::account::user { "gerbaux@root": user => "gerbaux", account => "root" }
 
   include c2corg::collectd::client
+
+  package { "libapache2-mod-proxy-html": }
+
+  apache::module { "proxy_html":
+    ensure  => present,
+    require => Package["libapache2-mod-proxy-html"]
+  }
+
+  apache::vhost { "admin-backends":
+    aliases => ["128.179.66.13"],
+  }
+
+  apache::proxypass { "pgfouine reports":
+    location => "/pgfouine",
+    url      => "http://192.168.191.126/pgfouine",
+    vhost    => "admin-backends",
+  }
+
+  apache::proxypass { "drraw rrd viewer":
+    location => "/drraw.cgi",
+    url      => "http://192.168.191.126/cgi-bin/drraw.cgi",
+    vhost    => "admin-backends",
+  }
+
+  apache::directive { "rewrite drraws hardcoded URLs":
+    vhost     => "admin-backends",
+    directive => "
+SetOutputFilter proxy-html
+ProxyHTMLURLMap http://192.168.191.126/cgi-bin/drraw.cgi /drraw.cgi
+",
+    require   => Apache::Module["proxy_html"],
+  }
+
+  apache::auth::htpasswd { "c2corg@camptocamp.org":
+    vhost    => "admin-backends",
+    username => "c2corg",
+    cryptPassword => "UxYkCOe3sNVJc",
+  }
+
+  apache::auth::basic::file::user { "protect logs stats and monitoring":
+    vhost => "admin-backends",
+  }
+
 }
 
 node 'test-marc' inherits 'base-node' {
