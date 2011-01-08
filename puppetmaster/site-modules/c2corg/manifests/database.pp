04fe8c1ac0e9f06eeea7ecdab3c24890dda5271d
Marc Fournier <marc.fournier@camptocamp.org>
2011-01-08 21:22:43 +0000

    postgres: envoi logs sur le serveur de logs.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3466 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/database.pp
+++ puppetmaster/site-modules/c2corg/manifests/database.pp
@@ -26,8 +26,14 @@ class c2corg::database::base {
 
 class c2corg::database::prod inherits c2corg::database::base {
 
+  $pgver = "8.4"
+  $logfacility = "LOCAL0"
+
+  Augeas { context => "/files/etc/postgresql/${pgver}/main/" }
+  Postgresql::Hba { pgver => $pgver }
+  #Postgresql::Conf { pgver => $pgver }
+
   augeas { "passwd auth for local postgres connections":
-    context   => "/files/etc/postgresql/8.4/main/",
     changes   => "set pg_hba.conf/*[type='local'][user='all'][database='all']/method md5",
     load_path => "/usr/share/augeas/lenses/contrib/",
     notify    => Service["postgresql"],
@@ -41,7 +47,25 @@ class c2corg::database::prod inherits c2corg::database::base {
     user     => 'sympa',
     address  => '192.168.0.0/16',
     method   => 'md5',
-    pgver    => '8.4',
+  }
+
+  #TODO
+  #postgresql::conf {
+  #  "log_statement": value => "all";
+  #  "log_destination": value => "syslog";
+  #  "syslog_facility": value => $logfacility;
+  #  "log_line_prefix": value => "%t";
+  #  "log_min_duration_statement": value => "0";
+  #}
+
+  file { "/etc/rsyslog.d/zzz-discard-postgresql-logs.conf":
+    ensure  => present,
+    content => "# file managed by puppet
+# discard messages coming from postgresql
+${logfacility}.* ~
+",
+    notify  => Service["syslog"],
+    require => Package["syslog"],
   }
 
 }
