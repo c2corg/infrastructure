11d3daaced9ce4bf066f5021772a48b7d811461d
Marc Fournier <marc.fournier@camptocamp.org>
2011-01-20 07:56:00 +0000

    migrated hn2 collectd config to new system.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3498 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/manifests/nodes.pp
+++ puppetmaster/manifests/nodes.pp
@@ -77,7 +77,24 @@ node 'hn2' inherits 'base-node' {
 
   include haproxy
 
-  #include c2corg::collectd::client # warning: don't break existing collectd installation !!
+  include c2corg::collectd::client
+  include apache::collectd
+
+  collectd::plugin { "processes":
+    content => '# Avoid LoadPlugin as processes is already loaded elsewhere
+<Plugin processes>
+  Process "apache2"
+  Process "postgres"
+</Plugin>
+',
+  }
+
+  # Solution transitoire. Il faudrait trouver une solution qui s'intègre plus
+  # intelligemment dans l'infra, peut-être au niveau de haproxy ou varnish.
+  collectd::plugin { "httplogsc2corg":
+    content => template("c2corg/collectd/httplogsc2corg.conf"), # argh, file() doesn't handle puppet:// URLs :-(
+  }
+
 }
 
 node 'pre-prod' inherits 'base-node' {
