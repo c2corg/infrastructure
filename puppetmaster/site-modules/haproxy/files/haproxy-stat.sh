e9de87d624a5dd6b1d50d10475678e3eb29922a2
Marc Fournier <marc.fournier@camptocamp.org>
2011-01-21 13:18:52 +0000

    collectd: corrected typo.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3509 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/haproxy/files/haproxy-stat.sh
+++ puppetmaster/site-modules/haproxy/files/haproxy-stat.sh
@@ -16,7 +16,7 @@ done
 while [ $? -eq 0 ]; do
   time="$(date +%s)"
   echo 'show stat' | socat - UNIX-CLIENT:$sock \
-  | while IFS=',' read pxname svname qcur qmax scur smax slim stot bin bout dreq dresp ereq econ eresp wretr wredis status weight act bck chkfail chdown lastchg downtime qlimit pid iid sid throttle lbtot tracked type rate rate_lim rate_max check_status check_code check_duration hrsp_1xxhrsp_2xx hrsp_3xx hrsp_4xx hrsp_5xx hrsp_other hanafail req_rate req_rate_max req_tot cli_abrt srv_abrt; do
+  | while IFS=',' read pxname svname qcur qmax scur smax slim stot bin bout dreq dresp ereq econ eresp wretr wredis status weight act bck chkfail chdown lastchg downtime qlimit pid iid sid throttle lbtot tracked type rate rate_lim rate_max check_status check_code check_duration hrsp_1xx hrsp_2xx hrsp_3xx hrsp_4xx hrsp_5xx hrsp_other hanafail req_rate req_rate_max req_tot cli_abrt srv_abrt; do
     [ "$svname" != 'BACKEND' ] && continue
     echo "PUTVAL $host/haproxy/haproxy_backend-$pxname interval=$pause $time:${stot:-0}:${bin:-0}:${bout:-0}:${econ:-0}:${eresp:-0}:${hrsp_1xx:-0}:${hrsp_2xx:-0}:${hrsp_3xx:-0}:${hrsp_4xx:-0}:${hrsp_5xx:-0}:${hrsp_other:-0}"
   done
