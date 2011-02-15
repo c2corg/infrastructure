c141d069b1c777e35300d56637a168a083ab1dc9
Marc Fournier <marc.fournier@camptocamp.org>
2011-02-15 21:05:06 +0000

    collectd: commented values already defined in types.db.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3565 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/varnish.pp
+++ puppetmaster/site-modules/c2corg/manifests/varnish.pp
@@ -78,16 +78,16 @@ class c2corg::varnish::typesdb {
   file { "varnish collectd types":
     path    => $typesdb,
     content => "# file managed by puppet
-bytes     value:GAUGE:0:U
+#bytes     value:GAUGE:0:U
 cache_operation   value:DERIVE:0:U
-cache_result    value:DERIVE:0:U
-connections   value:DERIVE:0:U
-http_requests   value:DERIVE:0:U
+#cache_result    value:DERIVE:0:U
+#connections   value:DERIVE:0:U
+#http_requests   value:DERIVE:0:U
 requests    value:GAUGE:0:U
-threads     value:GAUGE:0:U
-total_bytes   value:DERIVE:0:U
+#threads     value:GAUGE:0:U
+#total_bytes   value:DERIVE:0:U
 total_operations  value:DERIVE:0:U
-total_requests    value:DERIVE:0:U
+#total_requests    value:DERIVE:0:U
 total_sessions    value:DERIVE:0:U
 total_threads   value:DERIVE:0:U
 ",
