d1d9892c913733ad2dd2a2ba918d7797a1332548
Marc Fournier <marc.fournier@camptocamp.org>
2011-02-17 21:01:38 +0000

    haproxy: we need to know how long log analysis takes.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3577 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/syslog.pp
+++ puppetmaster/site-modules/c2corg/manifests/syslog.pp
@@ -210,7 +210,7 @@ class c2corg::syslog::haproxy {
   delaycompress
   postrotate
     /usr/sbin/invoke-rc.d syslog-ng reload >/dev/null
-    RUBYLIB=/srv/syslog/haproxy/request-log-analyzer/lib/ ruby1.8 /srv/syslog/haproxy/request-log-analyzer/bin/request-log-analyzer --format /srv/syslog/haproxy/haproxy.rb --output html --report-amount 50 --silent --file /var/www/haproxy-logs/$(date +%Y%m%d-%Hh%Mm).html /srv/syslog/haproxy/prod.log.1 2>&1 | logger -t RLA
+    time RUBYLIB=/srv/syslog/haproxy/request-log-analyzer/lib/ ruby1.8 /srv/syslog/haproxy/request-log-analyzer/bin/request-log-analyzer --format /srv/syslog/haproxy/haproxy.rb --output html --report-amount 50 --silent --file /var/www/haproxy-logs/$(date +%Y%m%d-%Hh%Mm).html /srv/syslog/haproxy/prod.log.1 2>&1 | logger -t RLA
   endscript
 }
 ",
