1500e6bc82f4c9738d25d280900e3971cd4f5344
Marc Fournier <marc.fournier@camptocamp.org>
2011-02-20 09:39:22 +0000

    haproxy: still not the correct incantation.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3579 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/syslog.pp
+++ puppetmaster/site-modules/c2corg/manifests/syslog.pp
@@ -210,7 +210,7 @@ class c2corg::syslog::haproxy {
   delaycompress
   postrotate
     /usr/sbin/invoke-rc.d syslog-ng reload >/dev/null
-    /usr/bin/time /usr/bin/nice -n 19 RUBYLIB=/srv/syslog/haproxy/request-log-analyzer/lib/ ruby1.8 /srv/syslog/haproxy/request-log-analyzer/bin/request-log-analyzer --format /srv/syslog/haproxy/haproxy.rb --output html --report-amount 50 --silent --file /var/www/haproxy-logs/$(date +%Y%m%d-%Hh%Mm).html /srv/syslog/haproxy/prod.log.1 2>&1 | logger -t RLA
+    RUBYLIB=/srv/syslog/haproxy/request-log-analyzer/lib/ time nice -n 19 ruby1.8 /srv/syslog/haproxy/request-log-analyzer/bin/request-log-analyzer --format /srv/syslog/haproxy/haproxy.rb --output html --report-amount 50 --silent --file /var/www/haproxy-logs/$(date +%Y%m%d-%Hh%Mm).html /srv/syslog/haproxy/prod.log.1 2>&1 | logger -t RLA
   endscript
 }
 ",
