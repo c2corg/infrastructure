dc087bd2a5a806b2c6a6a00cce1bd3d37f8ed996
Marc Fournier <marc.fournier@camptocamp.org>
2011-02-21 20:28:17 +0000

    varnish: increased swap metadata limit, fix #722.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3583 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/hn.pp
+++ puppetmaster/site-modules/c2corg/manifests/hn.pp
@@ -68,6 +68,12 @@ fi
 
 /* varnish server */
 class c2corg::hn::hn1 inherits c2corg::hn {
+
+  exec { "Increase swap metadata space for huge swap": # see #722
+    command => 'sed -i -r "/\/boot\/kfreebsd/ a \\\tset kFreeBSD.kern.maxswzone=187406484" /boot/grub/grub.cfg',
+    unless  => "grep 'kFreeBSD.kern.maxswzone' /boot/grub/grub.cfg",
+  }
+
 }
 
 /* webserver & database */
