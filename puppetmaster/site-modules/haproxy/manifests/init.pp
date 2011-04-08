794425493080e3a39e8f86f5bcb324c50b1e9c86
Marc Fournier <marc.fournier@camptocamp.org>
2011-04-08 20:16:15 +0000

    haproxy: add option to monitor backends as well as frontend stats.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3682 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/haproxy/manifests/init.pp
+++ puppetmaster/site-modules/haproxy/manifests/init.pp
@@ -64,7 +64,7 @@ class haproxy::collectd {
     content => "# file managed by puppet
 Loadplugin \"exec\"
 <Plugin exec>
-  Exec \"haproxy:haproxy\" \"${plugin}\"
+  Exec \"haproxy:haproxy\" \"${plugin}\" \"-x\" \"c2corg\"
 </Plugin>
 ",
     require => [
@@ -86,7 +86,7 @@ class haproxy::collectd::typesdb {
 
   file { "haproxy collectd types":
     path    => $typesdb,
-    content => "haproxy_backend   stot:COUNTER:0:U   bin:COUNTER:0:U   bout:COUNTER:0:U   econ:COUNTER:0:U   eresp:COUNTER:0:U   hrsp_1xx:COUNTER:0:U   hrsp_2xx:COUNTER:0:U   hrsp_3xx:COUNTER:0:U   hrsp_4xx:COUNTER:0:U   hrsp_5xx:COUNTER:0:U   hrsp_other:COUNTER:0:U\n",
+    content => "haproxy   stot:COUNTER:0:U   bin:COUNTER:0:U   bout:COUNTER:0:U   econ:COUNTER:0:U   eresp:COUNTER:0:U   hrsp_1xx:COUNTER:0:U   hrsp_2xx:COUNTER:0:U   hrsp_3xx:COUNTER:0:U   hrsp_4xx:COUNTER:0:U   hrsp_5xx:COUNTER:0:U   hrsp_other:COUNTER:0:U\n",
   }
 
 }
