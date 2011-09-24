83eaa49618e3ae242d4c81c868718e7de5fb1774
Marc Fournier <marc.fournier@camptocamp.org>
2011-09-24 15:47:37 +0000

    preprod: improved auto-deploy procedure.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3939 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/preprod.pp
+++ puppetmaster/site-modules/c2corg/manifests/preprod.pp
@@ -1,5 +1,6 @@
 class c2corg::preprod::autoupdate {
 
+
   /* hackish stuff to autotomatically install and update c2corg codebase */
   vcsrepo { "/srv/www/camptocamp.org/":
     ensure   => "latest",
@@ -25,24 +26,29 @@ class c2corg::preprod::autoupdate {
     require => [Vcsrepo["/srv/www/camptocamp.org/"], Vcsrepo["/srv/www/meta.camptocamp.org"]],
   }
 
-  #TODO: having this file managed by puppet is a nuisance.
   file { "c2corg preprod.ini":
     path    => "/srv/www/camptocamp.org/deployment/preprod.ini",
-    source  => "puppet:///c2corg/symfony/preprod.ini",
+    source  => "file:///srv/www/camptocamp.org/deployment/conf.ini-dist",
     require => Vcsrepo["/srv/www/camptocamp.org/"],
     notify  => [Exec["c2corg refresh"], Exec["c2corg install"]],
   }
 
+  include c2corg::password
+  $db_user = "www-data"
+  $sitename = "pre-prod.dev.camptocamp.org"
+  $c2corg_vars = "DB_USER=${db_user};DB_PASS=${c2corg::password::pgsql};SERVER_NAME=www.${sitename};MOBILE_VERSION_HOST=m.${sitename};CLASSIC_VERSION_HOST=www.${sitename};STATIC_HOST=s.${sitename};STATIC_BASE_URL=http://s.${sitename}"
+
   exec { "c2corg install":
-    command => "php c2corg --install --conf preprod",
+    command => "C2CORG_VARS='${c2corg_vars}' php c2corg --install --conf preprod",
     cwd     => "/srv/www/camptocamp.org/",
     require => [File["/srv/www/camptocamp.org/cache"], File["/srv/www/camptocamp.org/log"]],
+    notify  => Service["apache"],
     logoutput   => true,
     refreshonly => true,
   }
 
   exec { "c2corg refresh":
-    command => "php c2corg --refresh --conf preprod --build",
+    command => "C2CORG_VARS='${c2corg_vars}' php c2corg --refresh --conf preprod --build",
     cwd     => "/srv/www/camptocamp.org/",
     require => Exec["c2corg install"],
     notify  => Service["apache"],
