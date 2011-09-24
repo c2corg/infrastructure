c1c104e18da5de7d83c6ef5544daf238526c8b85
Marc Fournier <marc.fournier@camptocamp.org>
2011-09-24 15:47:35 +0000

    small typo.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3938 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/webserver.pp
+++ puppetmaster/site-modules/c2corg/manifests/webserver.pp
@@ -16,7 +16,7 @@ class c2corg::webserver::symfony {
   include php
 
   apache::module { ["headers", "expires"]: }
-  package { ["php5-pgsql", "php5-gd", "php-pear"}
+  package { ["php5-pgsql", "php5-gd", "php-pear"]: }
   package { ["gettext", "msmtp"]: }
 
   # installed via an svn external from now on.
