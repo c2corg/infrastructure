d2ee819f131b97b82c8535df2cadeea89b376ca3
Marc Fournier <marc.fournier@camptocamp.org>
2011-09-24 14:59:24 +0000

    symfony framework is now installed via an svn-external.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3937 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/webserver.pp
+++ puppetmaster/site-modules/c2corg/manifests/webserver.pp
@@ -16,8 +16,11 @@ class c2corg::webserver::symfony {
   include php
 
   apache::module { ["headers", "expires"]: }
-  package { ["php5-pgsql", "php5-gd", "php-pear", "php-symfony"]: }
+  package { ["php5-pgsql", "php5-gd", "php-pear"}
   package { ["gettext", "msmtp"]: }
+
+  # installed via an svn external from now on.
+  package{ "php-symfony": ensure => absent }
 
   # fileinfo is included in recent PHPs
   if ($lsbdistcodename == 'lenny') {
@@ -30,14 +33,6 @@ class c2corg::webserver::symfony {
     notify  => Service["apache"],
   }
 
-  # this should no longer be necessary after migrating to a more recent symfony
-  # version.
-  exec { "remove incompatible php option":
-    command => "sed -i '/zend.ze1_compatibility_mode/ d' /usr/share/php/data/symfony/config/php.yml",
-    onlyif  => "grep -q 'zend.ze1_compatibility_mode' /usr/share/php/data/symfony/config/php.yml",
-    require => Package["php-symfony"],
-  }
-
   # workaround while http://code.google.com/p/paver/issues/detail?id=53
   # prevents from building a debian package of python-jstools.
   package { "python-setuptools": }
@@ -45,10 +40,6 @@ class c2corg::webserver::symfony {
     creates => "/usr/local/bin/jsbuild",
     require => Package["python-setuptools"],
   }
-}
-
-class c2corg::webserver::symfony::prod inherits c2corg::webserver::symfony {
-  Package[ "php-symfony"] { ensure => "1.0.11-1" }
 }
 
 class c2corg::webserver::carto {
