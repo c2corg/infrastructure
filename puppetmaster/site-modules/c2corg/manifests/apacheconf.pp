e9f3b05560b4847ef20d77983f34560577c17465
Marc Fournier <marc.fournier@camptocamp.org>
2011-11-02 07:54:51 +0000

    c2corg::apacheconf::prod - must serve requests on loopback interface too.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4102 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/apacheconf.pp
+++ puppetmaster/site-modules/c2corg/manifests/apacheconf.pp
@@ -37,6 +37,8 @@ class c2corg::apacheconf::prod inherits c2corg::apacheconf::common {
 
   # don't listen on *:80, This is haproxy's job.
   apache::listen { "${haproxy_apache_address}:80": ensure => present } # TODO: this is crap
+  apache::listen { "127.0.0.1:80": ensure => present } # needed by collectd
+
   include disable80
 
   class disable80 inherits apache::base {
