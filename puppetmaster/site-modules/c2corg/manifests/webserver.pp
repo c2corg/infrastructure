e90dab7cf13075611ec515ff3686299dbfae444c
Marc Fournier <marc.fournier@camptocamp.org>
2011-11-06 17:03:18 +0000

    c2corg::webserver - postfix config using gmail w/sasl auth as relay.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4142 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/webserver.pp
+++ puppetmaster/site-modules/c2corg/manifests/webserver.pp
@@ -37,13 +37,13 @@ class c2corg::webserver::symfony {
 
   apache::module { ["headers", "expires"]: }
   package { ["php5-pgsql", "php5-gd", "php-pear"]: }
-  package { ["gettext", "msmtp"]: }
+  package { "gettext": }
 
   #TODO: fix postgresql module to include a ::client class
   #package { "postgresql-client-common": } # psql should be available for diagnostics
 
-  # installed via an svn external from now on.
-  package{ "php-symfony": ensure => absent }
+  # stuff no longer required on this system class
+  package{ ["php-symfony", "msmtp"]: ensure => absent }
 
   # fileinfo is included in recent PHPs
   if ($lsbdistcodename == 'lenny') {
@@ -63,6 +63,40 @@ class c2corg::webserver::symfony {
   exec { "easy_install jstools":
     creates => "/usr/local/bin/jsbuild",
     require => Package["python-setuptools"],
+  }
+
+  include c2corg::password
+  $sender_relay_map = "/etc/postfix/sender_relay"
+  $sasl_pw_map      = "/etc/postfix/sasl_pw"
+
+  postfix::config {
+    "sender_dependent_relayhost_maps":      value => "hash:${sender_relay_map}";
+    "smtp_sasl_password_maps":              value => "hash:${sasl_pw_map}";
+    "smtp_sender_dependent_authentication": value => "yes";
+    "smtp_sasl_security_options":           value => "noanonymous";
+    "smtp_sasl_auth_enable":                value => "yes";
+    "smtp_use_tls":                         value => "yes";
+  }
+
+  line { "noreply@camptocamp.org sender relay":
+    file    => "${sender_relay_map}",
+    line    => "noreply@camptocamp.org    [smtp.gmail.com]:587",
+    notify  => Postfix::Hash["${sender_relay_map}"],
+    require => Package["postfix"],
+  }
+
+  package{ "libsasl2-modules": notify => Service["postfix"] }
+
+  line { "smtp.gmail.com submission credentials":
+    file    => "${sasl_pw_map}",
+    line    => "smtp.gmail.com    noreply@camptocamp.org:${c2corg::password::noreply_pass}",
+    notify  => Postfix::Hash["${sasl_pw_map}"],
+    require => Package["postfix"],
+  }
+
+  postfix::hash {
+    "${sender_relay_map}":  before => Postfix::Config["sender_dependent_relayhost_maps"];
+    "${sasl_pw_map}":       before => Postfix::Config["smtp_sasl_password_maps"];
   }
 }
 
