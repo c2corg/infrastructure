3cfe94197ba3ff3a345888d12d03507adc41d848
Marc Fournier <marc.fournier@camptocamp.org>
2011-11-06 21:39:46 +0000

    content-factory: symfony conf stuff.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4155 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ puppetmaster/site-modules/c2corg/manifests/webserver/symfony/content-factory.pp
@@ -0,0 +1,27 @@
+class c2corg::webserver::symfony::content-factory inherits c2corg::webserver::symfony {
+
+  include c2corg::password
+  $sitename = "content-factory.dev.camptocamp.org"
+
+  File["c2corg conf.ini"] {
+    path   => "/srv/www/camptocamp.org/deployment/content-factory.ini",
+    owner  => "c2corg",
+    group  => "c2corg",
+  }
+
+  File["/home/c2corg/c2corg-envvars.sh"] {
+    content => inline_template("# file managed by puppet
+<%
+  c2corg_vars = {
+    'DB_USER'               => '${c2corg::password::www_db_user}',
+    'DB_PASS'               => '${c2corg::password::dev_db_pass}',
+    'SERVER_NAME'           => 'www.${sitename}',
+    'MOBILE_VERSION_HOST'   => 'm.${sitename}',
+    'CLASSIC_VERSION_HOST'  => 'www.${sitename}',
+  }
+%>
+export C2CORG_VARS='<%= c2corg_vars.map{ |k,v| \"#{k}=#{v}\" }.join(';') %>'
+"),
+  }
+
+}
