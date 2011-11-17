4ce24af53a92db8ad6c86cde184824f9267f594b
Marc Fournier <marc.fournier@camptocamp.org>
2011-11-17 21:28:35 +0000

    cleanup + ajout ersatz VMs pour josë et bubu.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4212 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/manifests/nodes.pp
+++ puppetmaster/manifests/nodes.pp
@@ -281,3 +281,35 @@ node 'test-xbrrr' inherits 'base-node' {
   include c2corg::apacheconf::dev
 
 }
+
+# VM
+node 'test-jose' inherits 'base-node' {
+
+  realize C2corg::Account::User['jose']
+  c2corg::account::user { "jose@root": user => "jose", account => "root" }
+
+  include c2corg::database::dev
+
+  #include c2corg::webserver::symfony::dev
+  include c2corg::webserver::carto
+  include c2corg::webserver::svg
+
+  include c2corg::apacheconf::dev
+
+}
+
+# VM
+node 'test-bubu' inherits 'base-node' {
+
+  #realize C2corg::Account::User['bubu']
+  #c2corg::account::user { "bubu@root": user => "bubu", account => "root" }
+
+  #include c2corg::database::dev
+
+  #include c2corg::webserver::symfony::dev
+  #include c2corg::webserver::carto
+  #include c2corg::webserver::svg
+
+  #include c2corg::apacheconf::dev
+
+}
--- puppetmaster/site-modules/c2corg/manifests/vz.pp
+++ puppetmaster/site-modules/c2corg/manifests/vz.pp
@@ -24,53 +24,39 @@ class c2corg::vz {
       vz::ve { "201": hname => "test-alex.pse.infra.camptocamp.org" }
       vz::ve { "202": hname => "test-xbrrr.pse.infra.camptocamp.org" }
       vz::ve { "203": hname => "test-marc.pse.infra.camptocamp.org" }
+      vz::ve { "204": hname => "test-jose.pse.infra.camptocamp.org" }
+      vz::ve { "205": hname => "test-bubu.pse.infra.camptocamp.org" }
 
       /*
        * port-forwards setup
        */
 
       vz::fwd {
-        "forward puppetmaster port":     ve => "101", from => "8140",  to => "8140";
-        "forward puppetmaster ssh port": ve => "101", from => "10101", to => "22";
+        "forward puppetmaster port": ve => "101", from => "8140",  to => "8140";
       }
 
       vz::fwd {
         "forward lists smtp port": ve => "102", from => "25",    to => "25";
-        "forward lists ssh port":  ve => "102", from => "10102", to => "22";
       }
 
       vz::fwd {
-        "forward dev ssh port":   ve => "103", from => "10103", to => "22";
         "forward dev https port": ve => "103", from => "443",   to => "443";
         "forward dev http port":  ve => "103", from => "80",    to => "80";
       }
 
       vz::fwd {
-        "forward pkg ssh port": ve => "125", from => "10125", to => "22";
-      }
-
-      vz::fwd {
-        "forward monit ssh port": ve => "126", from => "10126", to => "22";
-      }
-
-      vz::fwd {
+        "forward puppetmaster ssh port":    ve => "101", from => "10101", to => "22";
+        "forward lists ssh port":           ve => "102", from => "10102", to => "22";
+        "forward dev ssh port":             ve => "103", from => "10103", to => "22";
+        "forward pkg ssh port":             ve => "125", from => "10125", to => "22";
+        "forward monit ssh port":           ve => "126", from => "10126", to => "22";
         "forward content-factory ssh port": ve => "140", from => "10140", to => "22";
-      }
-
-      vz::fwd {
-        "forward pre-prod ssh port": ve => "141", from => "10141", to => "22";
-      }
-
-      vz::fwd {
-        "forward test-alex ssh port": ve => "201", from => "10201", to => "22";
-      }
-
-      vz::fwd {
-        "forward test-xbrrr ssh port": ve => "202", from => "10202", to => "22";
-      }
-
-      vz::fwd {
-        "forward test-marc ssh port": ve => "203", from => "10203", to => "22";
+        "forward pre-prod ssh port":        ve => "141", from => "10141", to => "22";
+        "forward test-alex ssh port":       ve => "201", from => "10201", to => "22";
+        "forward test-xbrrr ssh port":      ve => "202", from => "10202", to => "22";
+        "forward test-marc ssh port":       ve => "203", from => "10203", to => "22";
+        "forward test-jose ssh port":       ve => "204", from => "10204", to => "22";
+        "forward test-bubu ssh port":       ve => "205", from => "10205", to => "22";
       }
     }
 
