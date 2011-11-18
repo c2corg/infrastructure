bd5da63b0438217a72b1f3630fea5e01377d2518
Marc Fournier <marc.fournier@camptocamp.org>
2011-11-18 16:30:50 +0000

    unbound: a caching DNS resolver.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4224 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ puppetmaster/site-modules/unbound/manifests/init.pp
@@ -0,0 +1,47 @@
+class unbound {
+
+  package { "unbound": ensure => present }
+
+  exec { "unbound-control-setup":
+    require => Package["unbound"],
+    creates => [
+      "/etc/unbound/unbound_control.pem",
+      "/etc/unbound/unbound_server.pem",
+    ]
+  }
+
+  exec { "fetch named.cache":
+    command => "curl -s ftp://FTP.INTERNIC.NET/domain/named.cache > /etc/unbound/named.cache",
+    creates => "/etc/unbound/named.cache",
+    require => [Package["curl"], Package["unbound"]],
+    notify  => Service["unbound"],
+  }
+
+  service { "unbound":
+    ensure    => running,
+    enable    => true,
+    hasstatus => false,
+    pattern   => "/usr/sbin/unbound",
+    require   => Package["unbound"],
+  }
+
+  file { "/etc/unbound/unbound.conf":
+    content => '# file managed by puppet
+server:
+  verbosity: 1
+  statistics-cumulative: yes
+  extended-statistics: yes
+  interface: 0.0.0.0
+  access-control: 192.168.0.0/16 allow
+  chroot: ""
+  root-hints: "/etc/unbound/named.cache"
+  private-domain: "infra.camptocamp.org"
+remote-control:
+  control-enable: yes
+  control-interface: 127.0.0.1
+  control-port: 953
+',
+    notify  => Service["unbound"],
+    require => Package["unbound"],
+  }
+}
