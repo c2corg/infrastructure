2007a5027872f0ae7e2e170e3010d150a43fe6d9
Marc Fournier <marc.fournier@camptocamp.org>
2012-02-14 06:32:00 +0000

    carbon collectd plugin.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4389 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/graphite/manifests/collectd.pp
+++ puppetmaster/site-modules/graphite/manifests/collectd.pp
@@ -6,4 +6,25 @@ class graphite::collectd {
     source   => "git://github.com/joemiller/collectd-graphite.git",
   }
 
+  collectd::plugin { "collectd-graphite":
+    require => [Vcsrepo["/usr/src/collectd-graphite/"], Service["carbon"]],
+    content => '# file managed by puppet
+<LoadPlugin "perl">
+  Globals true
+</LoadPlugin>
+
+<Plugin "perl">
+  BaseName "Collectd::Plugins"
+  LoadPlugin "Graphite"
+
+    <Plugin "Graphite">
+      Buffer "256000"
+      Prefix "collectd"
+      Host   "localhost"
+      Port   "2003"
+    </Plugin>
+</Plugin>
+',
+  }
+
 }
