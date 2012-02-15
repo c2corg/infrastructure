026a96f2eb71efa60a03664b4ffb1d6a36808a51
Marc Fournier <marc.fournier@camptocamp.org>
2012-02-15 21:47:25 +0000

    new memcachedb module.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4399 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ puppetmaster/site-modules/memcachedb/manifests/init.pp
@@ -0,0 +1,26 @@
+class memcachedb {
+
+  package { "memcachedb": ensure => present }
+
+  service { "memcachedb":
+    ensure    => running,
+    hasstatus => false,
+    enable    => true,
+    require   => Package["memcachedb"],
+  }
+
+  file { "/var/lib/memcachedb":
+    ensure  => directory,
+    owner   => "memcachedb",
+    group   => "memcachedb",
+    require => Package["memcachedb"],
+    before  => Service["memcachedb"],
+  }
+
+  file { "/etc/memcachedb.conf":
+    ensure => present,
+    source => "puppet:///memcachedb/memcachedb.conf",
+    notify => Service["memcachedb"],
+  }
+
+}
