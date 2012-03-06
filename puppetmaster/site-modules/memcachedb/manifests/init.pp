58d5b213872bb6c038b9b489461bc4ed0f1e24fd
Marc Fournier <marc.fournier@camptocamp.org>
2012-03-06 08:15:45 +0000

    memcachedb: run db_archive with cron.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4484 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/memcachedb/manifests/init.pp
+++ puppetmaster/site-modules/memcachedb/manifests/init.pp
@@ -24,4 +24,14 @@ class memcachedb {
     require => Package["memcachedb"],
   }
 
+  cron { "db_archive":
+    command => 'echo -e "db_archive\r\nquit" | nc -q 2 localhost 11211 | logger -t "memcachedb db_archive" 2>&1',
+    user    => "memcachedb",
+    minute  => "34",
+    hour    => "*/4",
+    require => Service["memcachedb"],
+  }
+
+  #TODO: logrotate without restarting, memcachedb doesn't like -HUP
+
 }
