6ef38ee55299e861106b8cc38fbe57e5daaaadfe
Marc Fournier <marc.fournier@camptocamp.org>
2012-03-06 08:15:47 +0000

    postgres: lvm & filesystem settings, previously managed manually.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4486 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/prod/fs/postgres.pp
+++ puppetmaster/site-modules/c2corg/manifests/prod/fs/postgres.pp
@@ -1,6 +1,64 @@
 class c2corg::prod::fs::postgres {
 
-# TODO: manage LVM & filesystems
+  # /tmp
+  logical_volume { "tmp":
+    ensure       => present,
+    volume_group => "vg0",
+    initial_size => "1G",
+  }
+
+  filesystem { "/dev/mapper/vg0-tmp":
+    ensure  => present,
+    fs_type => "ext3",
+    require => Logical_volume["tmp"],
+  }
+
+  mount { "/tmp":
+    ensure  => mounted,
+    atboot  => true,
+    fstype  => "ext3",
+    options => "defaults",
+    device  => "/dev/mapper/vg0-tmp",
+    require => Filesystem["/dev/mapper/vg0-tmp"],
+    before  => File["/tmp"],
+  }
+
+
+  # /var/log
+  logical_volume { "log":
+    ensure       => present,
+    volume_group => "vg0",
+    initial_size => "3G",
+  }
+
+  filesystem { "/dev/mapper/vg0-log":
+    ensure  => present,
+    fs_type => "ext3",
+    require => Logical_volume["log"],
+  }
+
+  mount { "/var/log":
+    ensure  => mounted,
+    atboot  => true,
+    fstype  => "ext3",
+    options => "defaults",
+    device  => "/dev/mapper/vg0-log",
+    require => Filesystem["/dev/mapper/vg0-log"],
+  }
+
+
+  # /var/lib/postgresql/NNN/main
+  logical_volume { "pgdata":
+    ensure       => present,
+    volume_group => "vg0",
+    initial_size => "20G",
+  }
+
+  filesystem { "/dev/mapper/vg0-pgdata":
+    ensure  => present,
+    fs_type => "ext3",
+    require => Logical_volume["pgdata"],
+  }
 
   mount { "/var/lib/postgresql/8.4/main":
     ensure  => mounted,
@@ -8,6 +66,21 @@ class c2corg::prod::fs::postgres {
     device  => "/dev/mapper/vg0-pgdata",
     fstype  => "ext3",
     options => "noatime",
+    require => Filesystem["/dev/mapper/vg0-pgdata"],
+  }
+
+
+  # /var/lib/postgresql/NNN/main/pg_xlog
+  logical_volume { "pgxlog":
+    ensure       => present,
+    volume_group => "vg0",
+    initial_size => "5G",
+  }
+
+  filesystem { "/dev/mapper/vg0-pgxlog":
+    ensure  => present,
+    fs_type => "xfs",
+    require => Logical_volume["pgxlog"],
   }
 
   mount { "/var/lib/postgresql/8.4/main/pg_xlog":
@@ -16,8 +89,25 @@ class c2corg::prod::fs::postgres {
     device  => "/dev/mapper/vg0-pgxlog",
     fstype  => "xfs",
     options => "noatime,nobarrier",
-    require => Mount["/var/lib/postgresql/8.4/main"],
+    require => [
+      Mount["/var/lib/postgresql/8.4/main"],
+      Filesystem["/dev/mapper/vg0-pgxlog"],
+    ],
     before  => Service["postgresql"],
+  }
+
+
+  # /var/backups/pgsql
+  logical_volume { "pgbackup":
+    ensure       => present,
+    volume_group => "vg0",
+    initial_size => "10G",
+  }
+
+  filesystem { "/dev/mapper/vg0-pgbackup":
+    ensure  => present,
+    fs_type => "ext3",
+    require => Logical_volume["pgbackup"],
   }
 
   mount { "/var/backups/pgsql":
@@ -26,6 +116,7 @@ class c2corg::prod::fs::postgres {
     device  => "/dev/mapper/vg0-pgbackup",
     fstype  => "ext3",
     options => "defaults",
+    require => Filesystem["/dev/mapper/vg0-pgbackup"],
   }
 
 }
