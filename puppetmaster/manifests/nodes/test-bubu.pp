ad4810177168b4e96694b2890679cc8024013143
Marc Fournier <marc.fournier@camptocamp.org>
2012-04-26 21:06:12 +0000

    serveur de dev pour bubu.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4799 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/manifests/nodes/test-bubu.pp
+++ puppetmaster/manifests/nodes/test-bubu.pp
@@ -1,23 +1,22 @@
 # VM
 node 'test-bubu' inherits 'base-node' {
 
-  #$developer = "bubu"
+  $developer = "bubu"
 
-  #realize C2corg::Account::User['bubu']
-  #c2corg::account::user { "bubu@root": user => "bubu", account => "root" }
+  realize C2corg::Account::User['bubu']
+  c2corg::account::user { "bubu@root": user => "bubu", account => "root" }
 
-  #include c2corg::database::dev
+  include c2corg::database::dev
 
-  #include c2corg::webserver::symfony::dev
-  #include c2corg::webserver::carto
-  #include c2corg::webserver::svg
+  include c2corg::webserver::symfony::dev
+  include c2corg::webserver::carto
+  include c2corg::webserver::svg
 
-  #include c2corg::apacheconf::dev
+  include c2corg::apacheconf::dev
 
   fact::register {
     'role': value => 'dev';
     'duty': value => 'dev';
   }
-
 
 }
