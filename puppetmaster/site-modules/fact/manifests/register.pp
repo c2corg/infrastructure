39b3721fb457edb32df9e8a72dbe995dc644d336
Marc Fournier <marc.fournier@camptocamp.org>
2012-07-17 20:31:23 +0000

    fact::register - missing json lib.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5084 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/fact/manifests/register.pp
+++ puppetmaster/site-modules/fact/manifests/register.pp
@@ -2,6 +2,6 @@ define fact::register($value) {
 
   file { "/etc/facter/facts.d/${name}.json":
     ensure  => present,
-    content => inline_template('<%= { name, value.is_a?(Array) ? value.join(",") : value }.to_json %>'),
+    content => inline_template('<%= require "json"; { name, value.is_a?(Array) ? value.join(",") : value }.to_json %>'),
   }
 }
