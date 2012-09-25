fc1256b421b00f97cc45220e7b708dcd637e1a0d
Marc Fournier <marc.fournier@camptocamp.org>
2012-09-25 20:53:33 +0000

    $ipaddress is unreliable on nodes with several interfaces, base fact on $vlans instead.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5152 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/lib/facter/datacenter.rb
+++ puppetmaster/site-modules/c2corg/lib/facter/datacenter.rb
@@ -1,11 +1,20 @@
-if addr = Facter.value(:ipaddress)
-  Facter.add("datacenter") do
+vlan = Facter.value(:vlans)
+addr = Facter.value(:ipaddress)
+
+if vlan.is_a?(String) and vlan.split(',').include?('192')
+  Facter.add('datacenter') do
+    setcode do
+      'c2corg'
+    end
+  end
+else
+  Facter.add('datacenter') do
     setcode do
       case addr
-        when /^128\.179\.66/ : "epnet"
-        when /^95\.142/ : "gandi"
-        when /^192\.168\.1/ : "c2corg"
-        else "other"
+        when /^192\.168\.192/ : 'c2corg'
+        when /^128\.179\.66/ : 'epnet'
+        when /^95\.142/ : 'gandi'
+        else 'other'
       end
     end
   end
