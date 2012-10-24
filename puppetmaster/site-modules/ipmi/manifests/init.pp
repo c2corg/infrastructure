ff634381bc7e1650901f6635234166efc8b77288
Marc Fournier <marc.fournier@camptocamp.org>
2012-10-24 10:46:46 +0000

    ipmi: make this stuff work once an for all...
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5216 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/ipmi/manifests/init.pp
+++ puppetmaster/site-modules/ipmi/manifests/init.pp
@@ -2,11 +2,41 @@ class ipmi {
 
   package { 'ipmitool': ensure => present }
 
-  kmod::install { ['ipmi_devintf', 'ipmi_si']:
-    ensure => present,
-    before => Collectd::Plugin['ipmi'],
+  kmod::load { ['ipmi_devintf', 'ipmi_si']:
+    before => [Service['ipmievd'], Collectd::Plugin['ipmi']],
+    notify => Service['collectd'],
+  }
+
+  etcdefault { 'enable ipmievd service':
+    key    => 'ENABLED',
+    file   => 'ipmievd',
+    value  => 'true',
+    before => Service['ipmievd'],
+  }
+
+  service { 'ipmievd':
+    ensure    => 'running',
+    enable    => true,
+    hasstatus => false,
+    require   => Package['ipmitool'],
   }
 
   collectd::plugin { 'ipmi': lines => [] }
+
+  augeas { 'remove module ipmi_devintf':
+    incl    => '/etc/modprobe.d/modprobe.conf',
+    lens    => 'Modprobe.lns',
+    changes => "rm install[. = 'ipmi_devintf']",
+    onlyif  => "match install[. = 'ipmi_devintf'] size > 0",
+    before => Kmod::Load['ipmi_devintf'],
+  }
+
+  augeas { 'remove module ipmi_si':
+    incl    => '/etc/modprobe.d/modprobe.conf',
+    lens    => 'Modprobe.lns',
+    changes => "rm install[. = 'ipmi_si']",
+    onlyif  => "match install[. = 'ipmi_si'] size > 0",
+    before => Kmod::Load['ipmi_si'],
+  }
 
 }
