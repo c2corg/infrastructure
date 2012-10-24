80c065e013158ed1dfa868f345b468e6fa9fcc53
Marc Fournier <marc.fournier@camptocamp.org>
2012-10-24 19:42:14 +0000

    make exported resource name non-ambiguous.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5221 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/nat/manifests/fwd.pp
+++ puppetmaster/site-modules/nat/manifests/fwd.pp
@@ -1,6 +1,6 @@
 define nat::fwd ($net="192.168.192", $host, $from, $to, $proto, $iface="eth2") {
 
-  iptables { "forward from ${from} to ${net}.${host}:${to}":
+  iptables { "nat::fwd ${name} (from ${from}/${proto} to ${net}.${host}:${to})":
     chain       => "PREROUTING",
     table       => "nat",
     proto       => $proto,
