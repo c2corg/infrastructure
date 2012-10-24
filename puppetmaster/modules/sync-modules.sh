bebf8078375006f5a2c2e96d90f48711101afff2
Marc Fournier <marc.fournier@camptocamp.org>
2012-10-24 10:46:45 +0000

    updated several modules.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5215 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/modules/sync-modules.sh
+++ puppetmaster/modules/sync-modules.sh
@@ -5,28 +5,27 @@ IFS=' ' echo -e "
 # format:
 # URL<one space>modulename<one space>commit<newline>
 
-git://github.com/camptocamp/puppet-augeas.git augeas 1afd076a1
+git://github.com/camptocamp/puppet-augeas.git augeas 3e208168d
 git://github.com/camptocamp/puppet-apt.git apt b3da98203
 git://github.com/camptocamp/puppet-openssl.git openssl 9c76d53
-git://github.com/camptocamp/puppet-rsyncd.git rsyncd 6b9902e4
-git://github.com/camptocamp/puppet-postfix.git postfix 9cfd68455
-git://github.com/camptocamp/puppet-sysctl.git sysctl bf068e0
+git://github.com/camptocamp/puppet-postfix.git postfix 0537c3b4
+git://github.com/duritong/puppet-sysctl.git sysctl 8f035ff5
 git://github.com/camptocamp/puppet-reprepro.git reprepro 9b77b8bc4
 
 git://github.com/kbarber/puppet-iptables.git iptables 9deddbbd
 git://github.com/bodepd/puppet-sudo.git sudo 9834ac93
 git://github.com/puppetlabs/puppetlabs-rabbitmq.git rabbitmq 6471580ef7
-git://github.com/puppetlabs/puppetlabs-vcsrepo.git vcsrepo 44291fc
+git://github.com/puppetlabs/puppetlabs-vcsrepo.git vcsrepo 109d181b
 git://github.com/puppetlabs/puppetlabs-lvm.git lvm 3b1f783
 git://github.com/puppetlabs/puppetlabs-stdlib.git stdlib 44e99a7c
 git://github.com/ripienaar/puppet-concat.git concat aaf2eeb027
-git://github.com/puppetlabs/hiera.git hiera d1b4f7f
+git://github.com/puppetlabs/hiera.git hiera 2938116ba
 git://github.com/puppetlabs/hiera-puppet.git hiera-puppet bccaddc11f
 
-git://github.com/camptocamp/puppet-varnish.git varnish 92e517a3
+git://github.com/camptocamp/puppet-varnish.git varnish 89e30831
 git://github.com/camptocamp/puppet-apache.git apache 7a3080ee3
 git://github.com/camptocamp/puppet-nginx.git nginx 38767a43
-git://github.com/camptocamp/puppet-postgresql.git postgresql db3ced1
+git://github.com/camptocamp/puppet-postgresql.git postgresql b1840a6c
 git://github.com/camptocamp/puppet-postgis.git postgis d82fab5
 git://github.com/camptocamp/puppet-mapserver.git mapserver 2eb908fa1d
 
@@ -34,9 +33,9 @@ git://github.com/camptocamp/puppet-collectd.git collectd 8d369096
 
 git://github.com/camptocamp/puppet-python.git python 91fe7554
 git://github.com/camptocamp/puppet-ruby.git ruby d7debdf
-git://github.com/camptocamp/puppet-php.git php 02a622d3
+git://github.com/camptocamp/puppet-php.git php da817b136
 git://github.com/camptocamp/puppet-buildenv.git buildenv 6ca5168395
-git://github.com/camptocamp/puppet-kmod.git kmod f071c397a
+git://github.com/camptocamp/puppet-kmod.git kmod bea065b60
 
 " | egrep -v '^$|^#' | while read url module commit; do
 
@@ -47,7 +46,10 @@ git://github.com/camptocamp/puppet-kmod.git kmod f071c397a
     git clone "$url" "$module"
   else
     echo -e "\n\n@@@ Updating module $module (commit $commit)\n"
-    (cd "$module" && git remote update origin && git checkout "$commit")
+    (cd "$module" && \
+     git remote update origin && \
+     git checkout "$commit" && \
+     git diff --stat origin/master)
   fi
 
 done
