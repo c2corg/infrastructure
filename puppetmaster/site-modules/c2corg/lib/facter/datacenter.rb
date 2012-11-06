822fbcecfb552408a1eb7eb507b0420efdb02652
Marc Fournier <marc.fournier@camptocamp.org>
2012-11-06 21:56:29 +0000

    ruby1.9 fix.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5262 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/lib/facter/datacenter.rb
+++ puppetmaster/site-modules/c2corg/lib/facter/datacenter.rb
@@ -11,9 +11,9 @@ else
   Facter.add('datacenter') do
     setcode do
       case addr
-        when /^192\.168\.192/ : 'c2corg'
-        when /^128\.179\.66/ : 'epnet'
-        when /^95\.142/ : 'gandi'
+        when /^192\.168\.192/ then 'c2corg'
+        when /^128\.179\.66/  then 'epnet'
+        when /^95\.142/       then 'gandi'
         else 'other'
       end
     end
