8195e45b48033bfe9195e1ee0eaae6e22d6f5398
Marc Fournier <marc.fournier@camptocamp.org>
2012-11-28 19:58:41 +0000

    replaced double quotes by single quotes.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5369 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2cinfra/manifests/containers.pp
+++ puppetmaster/site-modules/c2cinfra/manifests/containers.pp
@@ -1,30 +1,30 @@
 class c2cinfra::containers {
 
   case $::hostname {
-    "hn0": {
+    'hn0': {
 
-      vz::ve { "201": hname => "test-alex.pse.infra.camptocamp.org" }
-      vz::ve { "202": hname => "test-xbrrr.pse.infra.camptocamp.org" }
-      vz::ve { "203": hname => "test-marc.pse.infra.camptocamp.org" }
-      vz::ve { "204": hname => "test-jose.pse.infra.camptocamp.org" }
-      vz::ve { "205": hname => "test-bubu.pse.infra.camptocamp.org" }
-      vz::ve { "206": hname => "test-saimon.pse.infra.camptocamp.org" }
-      vz::ve { "221": hname => "dev-cda.pse.infra.camptocamp.org" }
+      vz::ve { '201': hname => 'test-alex.pse.infra.camptocamp.org' }
+      vz::ve { '202': hname => 'test-xbrrr.pse.infra.camptocamp.org' }
+      vz::ve { '203': hname => 'test-marc.pse.infra.camptocamp.org' }
+      vz::ve { '204': hname => 'test-jose.pse.infra.camptocamp.org' }
+      vz::ve { '205': hname => 'test-bubu.pse.infra.camptocamp.org' }
+      vz::ve { '206': hname => 'test-saimon.pse.infra.camptocamp.org' }
+      vz::ve { '221': hname => 'dev-cda.pse.infra.camptocamp.org' }
 
     }
 
-    "hn2": {
+    'hn2': {
 
-      vz::ve { "50":  hname => "dnscache.pse.infra.camptocamp.org" }
-      vz::ve { "55":  hname => "msgbroker.pse.infra.camptocamp.org" }
-      vz::ve { "70":  hname => "www-failover.pse.infra.camptocamp.org" }
-      vz::ve { "101": hname => "pm.pse.infra.camptocamp.org" }
-      vz::ve { "102": hname => "lists.pse.infra.camptocamp.org" }
-      vz::ve { "103": hname => "dev.pse.infra.camptocamp.org" }
-      vz::ve { "125": hname => "pkg.pse.infra.camptocamp.org" }
-      vz::ve { "126": hname => "monit.pse.infra.camptocamp.org" }
-      vz::ve { "140": hname => "content-factory.pse.infra.camptocamp.org" }
-      vz::ve { "141": hname => "pre-prod.pse.infra.camptocamp.org" }
+      vz::ve { '50':  hname => 'dnscache.pse.infra.camptocamp.org' }
+      vz::ve { '55':  hname => 'msgbroker.pse.infra.camptocamp.org' }
+      vz::ve { '70':  hname => 'www-failover.pse.infra.camptocamp.org' }
+      vz::ve { '101': hname => 'pm.pse.infra.camptocamp.org' }
+      vz::ve { '102': hname => 'lists.pse.infra.camptocamp.org' }
+      vz::ve { '103': hname => 'dev.pse.infra.camptocamp.org' }
+      vz::ve { '125': hname => 'pkg.pse.infra.camptocamp.org' }
+      vz::ve { '126': hname => 'monit.pse.infra.camptocamp.org' }
+      vz::ve { '140': hname => 'content-factory.pse.infra.camptocamp.org' }
+      vz::ve { '141': hname => 'pre-prod.pse.infra.camptocamp.org' }
 
     }
 
