9a762cca660ee2291eb2a7f7a20bcb6e04d67a76
Marc Fournier <marc.fournier@camptocamp.org>
2012-12-02 16:37:44 +0000

    activated hn5 as LXC container server.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5396 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/manifests/nodes/hn5.pp
+++ puppetmaster/manifests/nodes/hn5.pp
@@ -4,6 +4,10 @@ node 'hn5' inherits 'base-node' {
   include c2corg::hn::hn5
   include c2corg::collectd::node
 
+  class { 'c2corg::prod::fs::lxc': } ->
+  class { 'lxc::host': } ->
+  class { 'c2cinfra::containers': }
+
   fact::register {
     'role': value => 'HN lxc';
     'duty': value => 'prod';
