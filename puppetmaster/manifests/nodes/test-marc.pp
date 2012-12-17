eb85b0815add655d21cdf359a8ee4c46721d2e9e
Marc Fournier <marc.fournier@camptocamp.org>
2012-12-17 20:47:12 +0000

    setup varnish 3 migration environment.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5439 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/manifests/nodes/test-marc.pp
+++ puppetmaster/manifests/nodes/test-marc.pp
@@ -6,4 +6,18 @@ node 'test-marc' inherits 'base-node' {
     'duty': value => 'dev';
   }
 
+  sudoers { 'root access for marc':
+    users    => 'marc',
+    type     => 'user_spec',
+    commands => '(ALL) ALL',
+  }
+
+  include c2corg::varnish::instance
+
+  host {
+    'symfony-backend.c2corg': ip => '192.168.192.4';
+    'storage-backend.c2corg': ip => '192.168.192.70';
+  }
+
+
 }
