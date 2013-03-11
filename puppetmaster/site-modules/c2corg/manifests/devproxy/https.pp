31317c321b9175c7768ba05101f8800b8e5955be
Marc Fournier <marc.fournier@camptocamp.org>
2013-03-11 21:34:07 +0000

    remove haproxy from dashboard + move resources around for clarity.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5603 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- puppetmaster/site-modules/c2corg/manifests/devproxy/https.pp
+++ puppetmaster/site-modules/c2corg/manifests/devproxy/https.pp
@@ -12,23 +12,10 @@ class c2corg::devproxy::https {
     vhost    => "dev.camptocamp.org",
   }
 
-  c2corg::devproxy::dashboard { "haproxy stats":
-    location => "/haproxy-stats",
-    url      => "http://hn3.pse.infra.camptocamp.org:8008/stats",
-    vhost    => "dev.camptocamp.org",
-  }
-
   c2corg::devproxy::dashboard { "apache server-status":
     location => "/hn3-apache-server-status",
     url      => "http://hn3.pse.infra.camptocamp.org/server-status",
     vhost    => "dev.camptocamp.org",
-  }
-
-  c2corg::devproxy::dashboard { "drraw rrd viewer":
-    ensure   => absent,
-    location => "/cgi-bin/drraw.cgi",
-    url      => "http://monit.pse.infra.camptocamp.org/cgi-bin/drraw.cgi",
-    vhost    => "drraw.dev.camptocamp.org",
   }
 
   c2corg::devproxy::dashboard { "graphite viewer":
@@ -77,6 +64,22 @@ RequestHeader set ProxyUser %{PROXY_USER}e
     refreshonly => true,
   }
 
+  # list of facts to show in inventory
+  $facts = ['role', 'lsbdistcodename', 'virtual', 'datacenter', 'interfaces']
+
+  $nodes = pdbquery('nodes', ['=', ['node', 'active'], true ])
+  $res = pdbfactquery($nodes)
+
+  file { '/var/www/dev.camptocamp.org/htdocs/inventory.html':
+    content => template('c2cinfra/dashboard/inventory.erb'),
+  }
+
+  file { "/var/www/dev.camptocamp.org/private/dashboard-inventory.part":
+    content => "<li><a href='https://dev.camptocamp.org/inventory.html'>inventory</a></li>\n",
+    notify  => Exec["aggregate dashboard snippets"],
+  }
+
+
   # legacy stuff
   apache::directive { [
     "rewrite pgfouine hardcoded URLs",
@@ -94,20 +97,18 @@ RequestHeader set ProxyUser %{PROXY_USER}e
     ensure  => absent,
   }
 
-  # list of facts to show in inventory
-  $facts = ['role', 'lsbdistcodename', 'virtual', 'datacenter', 'interfaces']
-
-  $nodes = pdbquery('nodes', ['=', ['node', 'active'], true ])
-  $res = pdbfactquery($nodes)
-
-  file { '/var/www/dev.camptocamp.org/htdocs/inventory.html':
-    content => template('c2cinfra/dashboard/inventory.erb'),
+  c2corg::devproxy::dashboard { "haproxy stats":
+    ensure   => absent,
+    location => "/haproxy-stats",
+    url      => "http://hn3.pse.infra.camptocamp.org:8008/stats",
+    vhost    => "dev.camptocamp.org",
   }
 
-  file { "/var/www/dev.camptocamp.org/private/dashboard-inventory.part":
-    content => "<li><a href='https://dev.camptocamp.org/inventory.html'>inventory</a></li>\n",
-    notify  => Exec["aggregate dashboard snippets"],
+  c2corg::devproxy::dashboard { "drraw rrd viewer":
+    ensure   => absent,
+    location => "/cgi-bin/drraw.cgi",
+    url      => "http://monit.pse.infra.camptocamp.org/cgi-bin/drraw.cgi",
+    vhost    => "drraw.dev.camptocamp.org",
   }
-
 
 }
