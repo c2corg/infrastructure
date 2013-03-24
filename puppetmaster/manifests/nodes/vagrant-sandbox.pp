30f1948a9ad559d71c67d1187c14693018ed31d4
Marc Fournier <marc.fournier@camptocamp.org>
2013-03-24 18:06:00 +0100

    default vagrant configfile + puppet node.

---
+++ puppetmaster/manifests/nodes/vagrant-sandbox.pp
@@ -0,0 +1,10 @@
+node 'vagrant-sandbox' inherits 'base-node' {
+
+  # Uncomment the folloing section to setup a symfony web + posgresql
+  # database environment.
+  #
+  # class { 'c2corg::dev::env::symfony':
+  #   developer => 'vagrant',
+  # }
+
+}
