3b2c326979e6f77cbe45f029a083911a6923c684
Marc Fournier <marc.fournier@camptocamp.org>
2013-03-25 09:22:53 +0100

    add local IP address to vhost aliases This allows access through vagrant's port forward.

--- puppetmaster/site-modules/c2corg/manifests/apacheconf/dev.pp
+++ puppetmaster/site-modules/c2corg/manifests/apacheconf/dev.pp
@@ -1,6 +1,11 @@
 class c2corg::apacheconf::dev inherits c2corg::apacheconf {
 
   Apache::Vhost["camptocamp.org"] {
-    aliases +> [$::hostname, $::fqdn, "${::hostname}.dev.camptocamp.org"],
+    aliases +> [
+      $::hostname,
+      $::fqdn,
+      "${::hostname}.dev.camptocamp.org",
+      '127.127.127.127',
+    ],
   }
 }
