2b47e5f237abc681b6d75cc42ecdb7234dc08039
lbesson <lionel.besson@gmail.com>
2013-04-04 20:59:15 +0300

    Add several aliases to vhost

--- puppetmaster/site-modules/c2corg/manifests/apacheconf/dev.pp
+++ puppetmaster/site-modules/c2corg/manifests/apacheconf/dev.pp
@@ -3,8 +3,14 @@ class c2corg::apacheconf::dev inherits c2corg::apacheconf {
   Apache::Vhost["camptocamp.org"] {
     aliases +> [
       $::hostname,
+      "www.${::hostname}",
+      "m.${::hostname}",
+      "s.${::hostname}",
       $::fqdn,
       "${::hostname}.dev.camptocamp.org",
+      "www.${::hostname}.dev.camptocamp.org",
+      "m.${::hostname}.dev.camptocamp.org",
+      "s.${::hostname}.dev.camptocamp.org",
       'localhost',
     ],
   }
