b65c4737f66957241aa57a770906e788d5de6a86
Marc Fournier <marc.fournier@camptocamp.org>
2013-05-20 12:05:03 +0200

    KISS nginx module

---
+++ puppetmaster/site-modules/nginx/manifests/site.pp
@@ -0,0 +1,19 @@
+define nginx::site ($ensure=present, source=undef, content=undef) {
+
+  file { "/etc/nginx/sites-available/${name}.conf":
+    ensure  => $ensure,
+    source  => $source,
+    content => $content,
+    require => Package['nginx'],
+  } ->
+
+  file { "/etc/nginx/sites-enabled/${name}.conf":
+    ensure => $ensure ? {
+      'present' => link,
+      'absent'  => absent,
+    },
+    target => "../sites-available/${name}.conf",
+    notify => Exec['reload-nginx'],
+  }
+
+}
