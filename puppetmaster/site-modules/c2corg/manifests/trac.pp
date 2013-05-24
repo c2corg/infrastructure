4e6e941c3eb140e63844dc80f56021913d7afeec
Marc Fournier <marc.fournier@camptocamp.org>
2013-05-24 21:00:49 +0200

    trac: remove SVN stuff

--- puppetmaster/site-modules/c2corg/manifests/trac.pp
+++ puppetmaster/site-modules/c2corg/manifests/trac.pp
@@ -24,34 +24,10 @@ class c2corg::trac {
     require => Package['sqlite3'],
   }
 
-  package { ["libapache2-svn", "sqlite3", "graphviz", "libjs-jquery"]:
+  package { ["sqlite3", "graphviz", "libjs-jquery"]:
     ensure => present,
   }
 
-  apache::module { "dav_svn":
-    require => Package["libapache2-svn"],
-  }
-
-  apache::directive { "svn":
-    vhost     => "dev.camptocamp.org",
-    directive => "
-<Location /svn/c2corg>
-  DAV svn
-  SVNPath /srv/svn/repos/c2corg
-
-  AuthzSVNAccessFile /srv/svn/repos/c2corg/conf/svnaccess.conf
-
-  AuthType Basic
-  AuthName Subversion
-  AuthUserFile /srv/trac/projects/c2corg/conf/htpasswd
-
-  <LimitExcept GET PROPFIND OPTIONS REPORT>
-    Require valid-user
-  </LimitExcept>
-
-</Location>
-",
-  }
 
   @@host { "dev.camptocamp.org":
     ip  => $::ipaddress,
@@ -65,70 +41,6 @@ Alias /tracdocs/ /usr/share/pyshared/trac/htdocs/
 
 ScriptAlias /trac/c2corg /var/www/dev.camptocamp.org/cgi-bin/trac.cgi
 ",
-  }
-
-  file { "/srv/svn/repos/c2corg/hooks/post-commit":
-    owner   => "root",
-    group   => "root",
-    mode    => 0755,
-    require => Package["trac"],
-    content => '#!/bin/sh
-# file managed by puppet
-
-REPOS="$1"
-REV="$2"
-
-TRAC_ENV="/srv/trac/projects/c2corg/"
-
-/bin/sh /usr/share/doc/trac/contrib/trac-svn-hook "$REPOS" "$REV"
-',
-  }
-
-
-  # short_open_tag conflicts with <?xml ... headers in pre-commit hook
-  augeas { 'disable php short open tags':
-    incl    => '/etc/php5/cli/php.ini',
-    lens    => 'PHP.lns',
-    changes => 'set PHP/short_open_tag Off',
-    require => Package['php5-cli'],
-  }
-
-
-  file { "/srv/svn/repos/c2corg/hooks/pre-commit":
-    owner   => "root",
-    group   => "root",
-    mode    => 0755,
-    require => Augeas['disable php short open tags'],
-    content => '#!/bin/bash
-# file managed by puppet
-
-# copied from http://blueparabola.com/blog/subversion-commit-hooks-php
-
-REPOS="$1"
-TXN="$2"
-
-PHP="/usr/bin/php"
-SVNLOOK="/usr/bin/svnlook"
-AWK="/usr/bin/awk"
-GREP="/bin/egrep"
-SED="/bin/sed"
-
-CHANGED=`$SVNLOOK changed -t "$TXN" "$REPOS" | $GREP "^[U|A]" | $AWK \'{print $2}\' | $GREP \\.php$`
-
-for FILE in $CHANGED
-do
-    MESSAGE=`$SVNLOOK cat -t "$TXN" "$REPOS" "$FILE" | $PHP -l`
-    if [ $? -ne 0 ]
-    then
-        echo 1>&2
-        echo "***********************************" 1>&2
-        echo "PHP error in: $FILE:" 1>&2
-        echo `echo "$MESSAGE" | $SED "s| -| $FILE|g"` 1>&2
-        echo "***********************************" 1>&2
-        exit 1
-    fi
-done
-',
   }
 
 # trac upgrade notes:
