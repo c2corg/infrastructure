fdce212945a060e22ca8cef668b2a98e94c315ee
Marc Fournier <marc.fournier@camptocamp.org>
2013-05-24 21:53:04 +0200

    no longer need trac from backports

--- puppetmaster/site-modules/c2corg/manifests/trac.pp
+++ puppetmaster/site-modules/c2corg/manifests/trac.pp
@@ -1,11 +1,5 @@
 class c2corg::trac {
 
-  apt::preferences { "trac_from_bpo":
-    package  => "trac trac-git",
-    pin      => "release a=${::lsbdistcodename}-backports",
-    priority => "1010",
-  }
-
   package { ['trac', 'trac-accountmanager', 'trac-email2trac', 'trac-mastertickets', 'trac-wikirename', 'trac-git']:
     ensure  => present,
     require => Package['sqlite3'],
