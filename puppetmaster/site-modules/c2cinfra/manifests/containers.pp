ba26f9b38db9be5057a70734cd8b4fa89a545566
Marc Fournier <marc.fournier@camptocamp.org>
2013-05-26 10:53:33 +0200

    remove legacy resources

--- puppetmaster/site-modules/c2cinfra/manifests/containers.pp
+++ puppetmaster/site-modules/c2cinfra/manifests/containers.pp
@@ -6,8 +6,6 @@ class c2cinfra::containers {
       vz::ve { '201': hname => 'test-alex.pse.infra.camptocamp.org' }
       vz::ve { '202': hname => 'test-xbrrr.pse.infra.camptocamp.org' }
       vz::ve { '203': hname => 'test-marc.pse.infra.camptocamp.org' }
-      vz::ve { '204': hname => 'test-jose.pse.infra.camptocamp.org', ensure => absent }
-      vz::ve { '205': hname => 'test-bubu.pse.infra.camptocamp.org', ensure => absent }
       vz::ve { '206': hname => 'test-saimon.pse.infra.camptocamp.org' }
       vz::ve { '207': hname => 'test-gottferdom.pse.infra.camptocamp.org' }
 
@@ -15,16 +13,8 @@ class c2cinfra::containers {
 
     'hn2': {
 
-      vz::ve { '50':  hname => 'dnscache.pse.infra.camptocamp.org', ensure => absent }
-      vz::ve { '55':  hname => 'msgbroker.pse.infra.camptocamp.org', ensure => absent }
       vz::ve { '70':  hname => 'www-failover.pse.infra.camptocamp.org' }
-      vz::ve { '101': hname => 'pm.pse.infra.camptocamp.org', ensure => absent }
-      vz::ve { '102': hname => 'lists.pse.infra.camptocamp.org', ensure => absent }
-      vz::ve { '103': hname => 'dev.pse.infra.camptocamp.org', ensure => absent }
-      vz::ve { '125': hname => 'pkg.pse.infra.camptocamp.org', ensure => absent }
       vz::ve { '126': hname => 'monit.pse.infra.camptocamp.org' }
-      vz::ve { '140': hname => 'content-factory.pse.infra.camptocamp.org', ensure => absent }
-      vz::ve { '141': hname => 'pre-prod.pse.infra.camptocamp.org', ensure => absent }
 
     }
 
