fed867e9638bee1be993559217279cd7cbaa6068
Marc Fournier <marc.fournier@camptocamp.org>
2013-05-27 07:25:25 +0200

    fix broken symlink in solr package

--- puppetmaster/manifests/nodes/test-stef74.pp
+++ puppetmaster/manifests/nodes/test-stef74.pp
@@ -17,6 +17,11 @@ node 'test-stef74' inherits 'base-node' {
   package { 'solr-jetty':
     ensure => present,
   } ->
+  # workaround for bug https://bugs.launchpad.net/ubuntu/+source/lucene-solr/+bug/1099320
+  file { '/var/lib/jetty/webapps/solr':
+    ensure => link,
+    target => '/usr/share/solr/web',
+  } ->
   etcdefault {
     'start jetty at boot time': key => 'NO_START', value => '0';
     'bind jetty to public interface': key => 'JETTY_HOST', value => '0.0.0.0';
