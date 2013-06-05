bb6bf62f0e6342032874ce892106234128d51fef
Marc Fournier <marc.fournier@camptocamp.org>
2013-06-05 11:42:34 +0200

    workaround broken lsb, debbug#711174

--- puppetmaster/manifests/nodes/base-node.pp
+++ puppetmaster/manifests/nodes/base-node.pp
@@ -8,7 +8,6 @@ node 'base-node' {
   include c2cinfra::account
   include c2cinfra::mta
   include c2cinfra::ssh::sshd
-  include "c2cinfra::apt::${::lsbdistcodename}"
   include c2cinfra::common::packages
   include c2cinfra::common::services
   include c2cinfra::common::config
@@ -16,6 +15,39 @@ node 'base-node' {
   include c2cinfra::syslog::client
   include c2cinfra::sudo # TODO: only if package sudo is installed
   include vz::facts
+
+  # workaround for broken LSB facts
+  if $::lsbdistcodename == 'n/a' {
+
+    $pkgrepo = hiera('pkgrepo_host')
+    apt::sources_list { "c2corg":
+      content => "# file managed by puppet
+deb http://${pkgrepo}/c2corg/ wheezy main
+  ",
+    } ->
+
+    apt::sources_list { 'debian':
+      content => '# file managed by puppet
+deb http://http.debian.net/debian/ wheezy main contrib non-free
+deb http://security.debian.org/ wheezy/updates main contrib non-free
+deb http://http.debian.net/debian/ wheezy-proposed-updates main contrib non-free
+',
+    } ->
+
+    # see debbug#711174
+    apt::preferences { 'force working version of base-files':
+      package  => 'base-files',
+      pin      => 'release l=C2corg, a=wheezy',
+      priority => '1100',
+    } ->
+
+    exec { 'apt-get -y --force-yes install base-files':
+      require => Exec['apt-get_update'],
+    }
+
+  } else {
+    include "c2cinfra::apt::${::lsbdistcodename}"
+  }
 
   if str2bool($::vagrant) {
     realize C2cinfra::Account::User['vagrant']
