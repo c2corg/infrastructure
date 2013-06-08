1d2324d3ca587fb04668fda5767b91af962387a4
Marc Fournier <marc.fournier@camptocamp.org>
2013-06-08 15:31:06 +0200

    cleanup

--- puppetmaster/manifests/site.pp
+++ puppetmaster/manifests/site.pp
@@ -6,7 +6,6 @@ Exec { path => "/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin:/usr/local/sbin" }
 
 File {
   backup => main,
-  ignore => '.svn',
   owner  => root,
   group  => root,
   mode   => 0644,
