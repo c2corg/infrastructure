e4540f7e4bbab07c3a897c4f3ef6bb6e4a3b2a86
Marc Fournier <marc.fournier@camptocamp.org>
2013-08-19 20:04:03 +0200

    now install custom built collectd packages

--- puppetmaster/site-modules/c2cinfra/manifests/collectd/node.pp
+++ puppetmaster/site-modules/c2cinfra/manifests/collectd/node.pp
@@ -9,12 +9,10 @@ class c2cinfra::collectd::node {
     'FQDNLookup': value => 'false';
   }
 
-  if $::lsbdistcodename == 'squeeze' {
-    apt::preferences { 'collectd_from_bpo':
-      package  => 'collectd collectd-core collectd-dbg collectd-dev collectd-utils libcollectdclient-dev libcollectdclient0',
-      pin      => "release a=${::lsbdistcodename}-backports",
-      priority => '1010',
-    }
+  apt::preferences { 'collectd_from_c2corg':
+    package  => 'collectd collectd-core collectd-dbg collectd-dev collectd-utils libcollectdclient-dev libcollectdclient1',
+    pin      => "release l=C2corg, a=${::lsbdistcodename}",
+    priority => '1010',
   }
 
   collectd::plugin { [
