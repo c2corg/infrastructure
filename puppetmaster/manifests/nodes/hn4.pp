5510ea314fae37d3a59009a9c79fbf8ea3954d63
Marc Fournier <marc.fournier@camptocamp.org>
2013-11-14 12:30:22 +0100

    hn4 is no longer a postgresql server

--- puppetmaster/manifests/nodes/hn4.pp
+++ puppetmaster/manifests/nodes/hn4.pp
@@ -3,20 +3,20 @@ node 'hn4' inherits 'base-node' {
 
   include c2cinfra::hn::hn4
 
-  include c2corg::database::prod
-  include c2cinfra::filesystem::postgres84
-  include c2corg::prod::env::postgres
+  #include c2corg::database::prod
+  #include c2cinfra::filesystem::postgres84
+  #include c2corg::prod::env::postgres
 
   fact::register {
     'role': value => ['postgresql', 'main database'];
     'duty': value => 'prod';
   }
 
-  class { 'postgresql::backup':
-    backup_format => 'custom',
-    backup_dir    => '/var/backups/pgsql',
-  }
+  #class { 'postgresql::backup':
+  #  backup_format => 'custom',
+  #  backup_dir    => '/var/backups/pgsql',
+  #}
 
-  c2cinfra::backup::dir { '/var/backups/pgsql': }
+  #c2cinfra::backup::dir { '/var/backups/pgsql': }
 
 }
