e1ca73f742f1b455922b49fecc270de3b4dc7c40
Marc Fournier <marc.fournier@camptocamp.org>
2013-12-14 13:57:07 +0100

    db0: postgres slave node

---
+++ puppetmaster/manifests/nodes/db0.pp
@@ -0,0 +1,17 @@
+# VM
+node 'db0' inherits 'base-node' {
+
+  include c2corg::database::prod
+  include c2corg::database::replication::slave
+  include c2cinfra::filesystem::postgres91
+  include c2corg::prod::env::postgres
+
+  fact::register {
+    'role': value => ['postgresql', 'main database'];
+    'duty': value => 'prod';
+  }
+
+  #include c2cinfra::backup::postgresql
+  c2cinfra::backup::dir { '/var/backups/pgsql': }
+
+}
