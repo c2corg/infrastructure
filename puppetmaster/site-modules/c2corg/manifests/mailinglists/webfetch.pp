ef5c3253160d832841fe875cf067b33f509c6841
Marc Fournier <marc.fournier@camptocamp.org>
2014-01-20 08:58:17 +0100

    switch to using dedicated user + msmtp

--- puppetmaster/site-modules/c2corg/manifests/mailinglists/webfetch.pp
+++ puppetmaster/site-modules/c2corg/manifests/mailinglists/webfetch.pp
@@ -1,11 +1,36 @@
 class c2corg::mailinglists::webfetch {
 
+  user { 'webfetch':
+    ensure     => present,
+    managehome => true,
+  } ->
+
+  file { '/home/webfetch/.msmtprc':
+    ensure  => present,
+    mode    => '0600',
+    owner   => 'webfetch',
+    group   => 'webfetch',
+    content => '# file managed by puppet
+defaults
+syslog on
+
+account sympa
+host lists.pse.infra.camptocamp.org
+port 25
+from nobody@lists.camptocamp.org
+
+account default: sympa
+',
+  }
+
   include '::phantomjs'
+
+  package { 'msmtp': ensure => present } ->
 
   file { '/var/cache/meteofrance':
     ensure => directory,
-    owner  => 'nobody',
-    group  => 'nogroup',
+    owner  => 'webfetch',
+    group  => 'webfetch',
   } ->
 
   package { ['python-lxml', 'python-argparse']:
@@ -26,8 +51,8 @@ class c2corg::mailinglists::webfetch {
 
   cron { 'bulletin nivo':
     ensure  => present,
-    command => 'cd /usr/local/bin/ && ./meteofrance.py -m smtp 2>&1 | logger -i -t bulletin-nivo',
-    user    => 'nobody',
+    command => 'cd /usr/local/bin/ && ./meteofrance.py -m msmtp 2>&1 | logger -i -t bulletin-nivo',
+    user    => 'webfetch',
     minute  => 15,
     hour    => [8,10,12,16,17,18,19],
     month   => [10,11,12,01,02,03,04,05,06],
