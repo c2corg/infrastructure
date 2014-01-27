c01b6380cbbd3113a712d49b55be082c509256b1
Marc Fournier <marc.fournier@camptocamp.org>
2014-01-28 00:09:04 +0100

    basic logstash module

---
+++ puppetmaster/site-modules/logstash/manifests/init.pp
@@ -0,0 +1,16 @@
+class logstash {
+
+  package { 'logstash': ensure => present } ->
+
+  etcdefault { 'start logstash at boot':
+    file  => 'logstash',
+    key   => 'START',
+    value => 'yes',
+  } ~>
+
+  service { 'logstash':
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+  }
+}
