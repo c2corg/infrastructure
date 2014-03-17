21d6a151f4f7915be706d2b4c93697e1c0bf802f
Marc Fournier <marc.fournier@camptocamp.org>
2014-03-17 23:40:12 +0100

    make logstash node actually do stuff

--- puppetmaster/manifests/nodes/logstash.pp
+++ puppetmaster/manifests/nodes/logstash.pp
@@ -1,4 +1,12 @@
 # VM
 node 'logstash' inherits 'base-node' {
 
+  include '::logstash'
+  include '::nginx'
+  include '::c2corg::syslog::pgbadger'
+
+  c2cinfra::backup::dir {
+    ['/etc/logstash', '/srv/logs/postgresql/reports/' ]:
+  }
+
 }
