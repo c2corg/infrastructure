134e44c737e5975830e90724cae2f78626cdaf47
Marc Fournier <marc.fournier@camptocamp.org>
2014-03-17 23:39:23 +0100

    install & run pgbadger

---
+++ puppetmaster/site-modules/c2corg/manifests/syslog/pgbadger.pp
@@ -0,0 +1,39 @@
+class c2corg::syslog::pgbadger {
+
+  package { ['pgbadger', 'libtext-csv-xs-perl']: ensure => present }
+
+  file { ['/srv/logs', '/srv/logs/postgresql']:
+    ensure => directory,
+  }
+
+  file { '/srv/logs/postgresql/csvlogs':
+    ensure => directory,
+    owner  => 'logstash',
+    group  => 'logstash',
+  }
+
+  file { '/srv/logs/postgresql/reports':
+    ensure => directory,
+    owner  => 'nobody',
+    group  => 'nogroup',
+  }
+
+  cron { 'process yesterdays pgsql logs':
+    user    => 'nobody',
+    minute  => '15',
+    hour    => '00',
+    command => '/usr/bin/pgbadger -I -q /srv/logs/postgresql/csvlogs/pglog-$(/bin/date --date="yesterday" +\%d)-*.csv.gz -O /srv/logs/postgresql/reports/',
+  }
+
+  cron { 'purge old pgsql raw logs':
+    user    => 'logstash',
+    minute  => '45',
+    hour    => '00',
+    command => 'find /srv/logs/postgresql/csvlogs/ -type f -mtime +7 -delete',
+  }
+
+  ::nginx::site { 'pgbadger':
+    source => 'puppet:///modules/c2corg/nginx/pgbadger.conf',
+  }
+
+}
