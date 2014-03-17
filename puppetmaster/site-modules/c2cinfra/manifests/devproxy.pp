1118839b1c90fc6dcbcd08225b8d1fbc086b1f04
Marc Fournier <marc.fournier@camptocamp.org>
2014-03-18 00:08:21 +0100

    replace link to pgfouine by pgbadger

--- puppetmaster/site-modules/c2cinfra/manifests/devproxy.pp
+++ puppetmaster/site-modules/c2cinfra/manifests/devproxy.pp
@@ -45,7 +45,7 @@ auth_basic_user_file /srv/trac/projects/c2corg/conf/htpasswd;
 
   $dashboard = {
     '/inventory.html'                         => 'inventory',
-    '/pgfouine'                               => 'pgfouine reports',
+    '/pgreports'                              => 'pgbadger reports',
     '/haproxy-logs'                           => 'haproxy logs',
     'https://graphite.dev.camptocamp.org/'    => 'graphite viewer',
     'https://puppetboard.dev.camptocamp.org/' => 'puppet dashboard',
