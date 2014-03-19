9a3c5515ca77a3c5542b0207d30da790ad606075
Marc Fournier <marc.fournier@camptocamp.org>
2014-03-19 23:04:35 +0100

    gandi local DNS servers

--- puppetmaster/site-modules/data/manifests/gandi.pp
+++ puppetmaster/site-modules/data/manifests/gandi.pp
@@ -1,4 +1,5 @@
 # hiera lookup based on "datacenter" fact
 class data::gandi {
   $puppetmaster_host = '128.179.66.13'
+  $resolvers = ['217.70.186.193', '217.70.186.194', '2001:4b98:dc2:49::193', '8.8.8.8']
 }
