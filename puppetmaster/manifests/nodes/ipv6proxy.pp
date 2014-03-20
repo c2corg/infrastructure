51bf74b9c62828949124e447ea867e0d99b23699
Marc Fournier <marc.fournier@camptocamp.org>
2014-03-20 01:19:36 +0100

    vpn setup for new host

--- puppetmaster/manifests/nodes/ipv6proxy.pp
+++ puppetmaster/manifests/nodes/ipv6proxy.pp
@@ -3,6 +3,11 @@ node 'ipv6proxy' inherits 'base-node' {
 
   include c2corg::webserver::ipv6gw
 
+  class { 'c2cinfra::openvpn::client':
+    username => 'ipv6proxy-vpn',
+    password => hiera('ipv6proxy_vpn_password'),
+  }
+
   fact::register {
     'role': value => ['proxy ipv6'];
     'duty': value => 'prod';
