7747f7cb0dd5ffd6f11c11d4ef7fe950072522ef
Marc Fournier <marc.fournier@camptocamp.org>
2014-03-20 14:40:47 +0100

    switch nivo-bulletin to independent repo

--- puppetmaster/site-modules/c2corg/manifests/mailinglists/webfetch.pp
+++ puppetmaster/site-modules/c2corg/manifests/mailinglists/webfetch.pp
@@ -37,25 +37,25 @@ account default: sympa
     ensure => present,
   } ->
 
-  file { '/usr/local/bin/meteofrance.js':
-    ensure => present,
-    mode   => 755,
-    source => 'puppet:///modules/c2corg/meteofrance/meteofrance.js',
-  } ->
-
-  file { '/usr/local/bin/meteofrance.py':
-    ensure => present,
-    mode   => 755,
-    source => 'puppet:///modules/c2corg/meteofrance/meteofrance.py',
+  vcsrepo { '/srv/nivo-bulletins':
+    ensure   => present,
+    owner    => 'webfetch',
+    group    => 'webfetch',
+    provider => 'git',
+    source   => 'https://github.com/c2corg/nivo-bulletins.git',
   } ->
 
   cron { 'bulletin nivo':
     ensure  => present,
-    command => 'cd /usr/local/bin/ && ./meteofrance.py -m msmtp 2>&1 | logger -i -t bulletin-nivo',
+    command => 'cd /srv/nivo-bulletins && ./meteofrance.py -m msmtp 2>&1 | logger -i -t bulletin-nivo',
     user    => 'webfetch',
     minute  => 15,
     hour    => [8,10,12,16,17,18,19],
     month   => [10,11,12,01,02,03,04,05,06],
+  }
+
+  file { ['/usr/local/bin/meteofrance.js', '/usr/local/bin/meteofrance.py']:
+    ensure => absent,
   }
 
 }
