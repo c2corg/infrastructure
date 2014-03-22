6558ddbc49f21ea0231de7c8db7c17766c812b66
Marc Fournier <marc.fournier@camptocamp.org>
2014-03-22 10:28:17 +0100

    simplify apache logrotate operations

--- puppetmaster/site-modules/apache/manifests/init.pp
+++ puppetmaster/site-modules/apache/manifests/init.pp
@@ -170,9 +170,6 @@ class apache {
   # the following variables are used in template logrotate-httpd.erb
   $logrotate_paths = "${apache::params::root}/*/logs/*.log ${apache::params::log}/*log"
   $httpd_pid_file = "/var/run/apache2.pid"
-  $httpd_reload_cmd = "/etc/init.d/apache2 restart > /dev/null"
-  $awstats_condition = "-f /usr/share/doc/awstats/examples/awstats_updateall.pl -a -f /usr/lib/cgi-bin/awstats.pl"
-  $awstats_command = "/usr/share/doc/awstats/examples/awstats_updateall.pl -awstatsprog=/usr/lib/cgi-bin/awstats.pl -confdir=/etc/awstats now > /dev/null"
 
   file {"logrotate configuration":
     ensure => present,
