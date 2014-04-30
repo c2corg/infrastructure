8284710462e7b1c3989b150e0ea2de11a99a0a06
Marc Fournier <marc.fournier@camptocamp.org>
2014-04-30 21:41:05 +0200

    enable solr stuff on new vm

--- puppetmaster/manifests/nodes/solr.pp
+++ puppetmaster/manifests/nodes/solr.pp
@@ -1,6 +1,10 @@
 # VM
 node 'solr' inherits 'base-node' {
 
+  realize C2cinfra::Account::User['stef74']
+
+  include c2corg::solr
+
   fact::register {
     'role': value => ['solr search'];
     'duty': value => 'prod';
