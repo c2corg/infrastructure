414b3eec69172c23d2893e7082bd88ffd0474ec4
Marc Fournier <marc.fournier@camptocamp.org>
2014-05-03 22:11:01 +0200

    install and update salt from saltstack.com repos

--- puppetmaster/site-modules/salt/manifests/init.pp
+++ puppetmaster/site-modules/salt/manifests/init.pp
@@ -1,7 +1,23 @@
 class salt {
 
-  apt::pin { 'saltstack_from_bpo':
-    packages => 'salt-common salt-master salt-minion salt-syndic python-zmq libzmq3',
+  apt::key { 'F2AE6AB9':
+    key_source => 'http://debian.saltstack.com/debian-salt-team-joehealy.gpg.key',
+  }
+
+  apt::source { 'saltstack':
+    location => 'http://debian.saltstack.com/debian/',
+    release  => "${::lsbdistcodename}-saltstack-2014-01",
+    repos    => 'main',
+  }
+
+  apt::pin { 'saltstack':
+    packages   => 'salt-common salt-master salt-minion salt-syndic',
+    originator => "saltstack.com",
+    priority   => '1010',
+  }
+
+  apt::pin { 'zmq_from_bpo':
+    packages => 'python-zmq libzmq3',
     release  => "${::lsbdistcodename}-backports",
     priority => '1010',
   }
