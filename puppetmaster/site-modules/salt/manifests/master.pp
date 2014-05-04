562896b6f05676b9eb4ba112aa36a7aa7ec8ba87
Marc Fournier <marc.fournier@camptocamp.org>
2014-05-05 01:20:21 +0200

    salt master portforwards

--- puppetmaster/site-modules/salt/manifests/master.pp
+++ puppetmaster/site-modules/salt/manifests/master.pp
@@ -14,4 +14,21 @@ class salt::master {
   Exec <<| tag == 'saltstack' |>>
   File <<| tag == 'saltstack' |>>
 
+  @@nat::fwd { '001 forward salt master port':
+    host  => '101',
+    from  => '4505',
+    to    => '4505',
+    proto => 'tcp',
+    tag   => 'portfwd',
+  }
+
+  @@nat::fwd { '002 forward salt master port':
+    host  => '101',
+    from  => '4506',
+    to    => '4506',
+    proto => 'tcp',
+    tag   => 'portfwd',
+  }
+
+
 }
