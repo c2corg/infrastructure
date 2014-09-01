ef8454c7ec88a7adce974c9cc6b059889e07cc86
Marc Fournier <marc.fournier@camptocamp.org>
2014-09-02 00:45:42 +0200

    monitor network quality from backup server

--- puppetmaster/site-modules/c2cinfra/manifests/hn/backup0.pp
+++ puppetmaster/site-modules/c2cinfra/manifests/hn/backup0.pp
@@ -3,4 +3,11 @@ class c2cinfra::hn::backup0 inherits c2cinfra::hn {
 
   include hardware::raid::md
 
+  collectd::config::plugin { 'ping home':
+    plugin   => 'ping',
+    settings => '
+Host "128.179.66.1"
+',
+  }
+
 }
