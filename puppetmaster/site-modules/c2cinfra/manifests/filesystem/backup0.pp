54b6654a0c42311572b4d32c9fad252eb2d8141b
Marc Fournier <marc.fournier@camptocamp.org>
2014-09-10 12:07:54 +0200

    bump up TTL to 2h

--- puppetmaster/site-modules/c2cinfra/manifests/filesystem/backup0.pp
+++ puppetmaster/site-modules/c2cinfra/manifests/filesystem/backup0.pp
@@ -9,7 +9,7 @@ class c2cinfra::filesystem::backup0 {
   zfs { 'srv/backups': ensure => present }
 
   cron { 'zpool-riemann':
-    command => "/usr/bin/timeout -s 9 30 /usr/local/sbin/zpool-riemann.py ${riemann_host} 1440 srv",
+    command => "/usr/bin/timeout -s 9 30 /usr/local/sbin/zpool-riemann.py ${riemann_host} 7200 srv",
     minute  => '*/24',
     require => Class['hardware::raid::zfs'],
   }
