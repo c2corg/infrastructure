77ef7a5de0ebe86f9376b842ad3c7bd2088f3b2b
Marc Fournier <marc.fournier@camptocamp.org>
2014-10-22 21:41:56 +0200

    avoid storing config files in a ramdisk

--- puppetmaster/site-modules/riemann/manifests/dash.pp
+++ puppetmaster/site-modules/riemann/manifests/dash.pp
@@ -6,13 +6,13 @@ class riemann::dash {
     ensure => present,
   } ->
 
-  file { '/var/run/riemann-dash':
+  file { '/var/lib/riemann-dash':
     ensure => directory,
     owner  => 'riemann',
     group  => 'riemann',
   } ->
 
-  file { '/var/run/riemann-dash/config.rb':
+  file { '/var/lib/riemann-dash/config.rb':
     ensure  => present,
     content => '# file managed by puppet
 set :port, 8080
@@ -23,7 +23,7 @@ config[:ws_config] = "/etc/riemann/config.json"
   runit::service { 'riemann-dash':
     user    => 'riemann',
     group   => 'riemann',
-    rundir  => '/var/run/riemann-dash',
+    rundir  => '/var/lib/riemann-dash',
     logdir  => '/var/log/riemann-dash',
     command => '/usr/bin/riemann-dash 2>&1',
   }
