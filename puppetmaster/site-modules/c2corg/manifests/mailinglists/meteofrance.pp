64816248250d8cdf03eb6899d7989517290e8620
lbesson <lionel.besson@gmail.com>
2014-11-08 12:14:44 +0100

    update scenario mf+slf

--- puppetmaster/site-modules/c2corg/manifests/mailinglists/meteofrance.pp
+++ puppetmaster/site-modules/c2corg/manifests/mailinglists/meteofrance.pp
@@ -4,7 +4,7 @@ define c2corg::mailinglists::meteofrance($ensure='present', $listmaster, $hname)
 
   sympa::list { "meteofrance-${dept}":
     ensure     => $ensure,
-    send_from  => "meteofrance",
+    send_from  => "meteofrance_slf",
     subject    => "Bulletins Nivo MeteoFrance ${dept}",
     anon_name  => "Bulletin Nivo MF ${dept}",
     listmaster => $listmaster,
