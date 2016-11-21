46187ab7e1e3fcecceca47ac1c9e84ec59dec1de
Marc Fournier <marc.fournier@camptocamp.org>
2016-11-21 17:44:30 +0100

    give root access to amorvan on datamigration node

--- puppetmaster/manifests/nodes/datamigration.pp
+++ puppetmaster/manifests/nodes/datamigration.pp
@@ -3,6 +3,8 @@ node 'datamigration' {
 
   include c2cinfra::common
 
+  c2cinfra::account::user { 'amorvan@root': user => 'amorvan', account => 'root' }
+
   fact::register {
     'role': value => ['datamigration'];
     'duty': value => 'prod';
