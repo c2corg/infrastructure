aa45d72dde7ddcc171222956ec4b47b4c291a2e0
Marc Fournier <marc.fournier@camptocamp.org>
2016-11-22 18:10:04 +0100

    datamigration-pgsqlslave: enable offsite backups

--- puppetmaster/manifests/nodes/datamigration-pgsqlslave.pp
+++ puppetmaster/manifests/nodes/datamigration-pgsqlslave.pp
@@ -7,4 +7,17 @@ node 'datamigration-pgsqlslave' {
     'role': value => ['datamigration db replica'];
     'duty': value => 'prod';
   }
+
+  # nb: postgresql 9.6 installed and setup manually
+  class { 'postgresql::globals':
+    version => '9.6',
+  } ->
+  class { 'postgresql::server':
+    manage_pg_hba_conf   => false,
+    listen_addresses     => 'localhost',
+  }
+
+  include c2cinfra::backup::postgresql
+  c2cinfra::backup::dir { '/var/backups/pgsql': }
+
 }
