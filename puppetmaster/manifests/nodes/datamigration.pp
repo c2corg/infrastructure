e06fb13298164282720416e07c9086055754257b
Marc Fournier <marc.fournier@camptocamp.org>
2016-12-01 10:54:09 +0100

    give root access to alex and tsauerwein on datamigration node

--- puppetmaster/manifests/nodes/datamigration.pp
+++ puppetmaster/manifests/nodes/datamigration.pp
@@ -4,6 +4,8 @@ node 'datamigration' {
   include c2cinfra::common
 
   c2cinfra::account::user { 'amorvan@root': user => 'amorvan', account => 'root' }
+  c2cinfra::account::user { 'alex@root': user => 'alex', account => 'root' }
+  c2cinfra::account::user { 'tsauerwein@root': user => 'tsauerwein', account => 'root' }
 
   fact::register {
     'role': value => ['datamigration'];
